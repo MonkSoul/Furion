4e7467061a24521e3fbaafbcf384e623a555aa6c
Monk <monksoul@outlook.com>
2020-06-29 18:38:12 +0800

    link #I1LT5D

--- aspnetcore/Fur/AttachController/Dependencies/IAttachControllerDependency.cs
+++ aspnetcore/Fur/AttachController/Dependencies/IAttachControllerDependency.cs
@@ -1,7 +1,9 @@
-﻿namespace Fur.AttachController.Dependencies
+﻿using Fur.DependencyInjection.Lifetimes;
+
+namespace Fur.AttachController.Dependencies
 {
     /// <summary>
     /// 附加控制器依赖接口
     /// </summary>
-    public interface IAttachControllerDependency { }
+    public interface IAttachControllerDependency : ITransientLifetime { }
 }
