82926a3a048a2e86010dc92e75427a1cfe489186
百小僧 <MonkSoul@outlook.com>
2020-07-05 18:26:43 +0800

    link #I1LT5R

---
+++ aspnetcore/Fur/DatabaseVisitor/Repositories/IMultipleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fur.DatabaseVisitor.Dependencies;
+using Fur.DatabaseVisitor.Identifiers;
+
+namespace Fur.DatabaseVisitor.Repositories
+{
+    public interface IMultipleRepository
+    {
+        IMultipleRepositoryOfT<TEntity, TDbContextIdentifier> GetMultipleRepository<TEntity, TDbContextIdentifier>(bool newScope = false)
+            where TEntity : class, IEntity, new()
+            where TDbContextIdentifier : IDbContextIdentifier;
+    }
+}
---
+++ aspnetcore/Fur/DatabaseVisitor/Repositories/MultipleEFCoreRepository.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Fur.DatabaseVisitor.Dependencies;
+using Fur.DatabaseVisitor.Identifiers;
+using Fur.DependencyInjection.Lifetimes;
+using System;
+
+namespace Fur.DatabaseVisitor.Repositories
+{
+    public class MultipleEFCoreRepository : IMultipleRepository, IScopedLifetime
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public MultipleEFCoreRepository(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IMultipleRepositoryOfT<TEntity, TDbContextIdentifier> GetMultipleRepository<TEntity, TDbContextIdentifier>(bool newScope = false)
+            where TEntity : class, IEntity, new()
+            where TDbContextIdentifier : IDbContextIdentifier
+        {
+            if (newScope)
+            {
+                return _serviceProvider.GetAutofacRoot().BeginLifetimeScope().Resolve<IMultipleRepositoryOfT<TEntity, TDbContextIdentifier>>();
+            }
+            return _serviceProvider.GetAutofacRoot().Resolve<IMultipleRepositoryOfT<TEntity, TDbContextIdentifier>>();
+        }
+    }
+}
