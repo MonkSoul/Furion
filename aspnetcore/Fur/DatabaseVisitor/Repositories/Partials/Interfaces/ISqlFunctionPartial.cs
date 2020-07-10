d4da212c9728acc1310db8d22f4e400a199fb2e0
百小僧 <MonkSoul@outlook.com>
2020-07-10 22:43:49 +0800

    link #I1N0QN

--- aspnetcore/Fur/DatabaseVisitor/Repositories/Partials/Interfaces/ISqlFunctionPartial.cs
+++ aspnetcore/Fur/DatabaseVisitor/Repositories/Partials/Interfaces/ISqlFunctionPartial.cs
@@ -11,29 +11,134 @@ namespace Fur.DatabaseVisitor.Repositories
     /// <typeparam name="TEntity">实体类型</typeparam>
     public partial interface IRepositoryOfT<TEntity> where TEntity : class, IDbEntity, new()
     {
+        #region 执行标量函数 + TResult SqlScalarFunctionQuery<TResult>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns>结果类型值</returns>
+        TResult SqlScalarFunctionQuery<TResult>(string name, params object[] parameters);
+        #endregion
 
-        TResult SqlScalarFunctionQuery<TResult>(string name, params object[] parameters) where TResult : struct;
+        #region 执行标量函数 + Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
+        Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, params object[] parameters);
+        #endregion
 
-        Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, params object[] parameters) where TResult : struct;
+        #region 执行标量函数 + TResult SqlScalarFunctionQuery<TResult>(string name, object parameterModel)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns>结果类型值</returns>
+        TResult SqlScalarFunctionQuery<TResult>(string name, object parameterModel);
+        #endregion
 
-        TResult SqlScalarFunctionQuery<TResult>(string name, object parameterModel) where TResult : struct;
+        #region 执行标量函数 + Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, object parameterModel)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
+        Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, object parameterModel);
+        #endregion
 
-        Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, object parameterModel) where TResult : struct;
 
+        #region 执行表值函数 返回 DataTable + DataTable SqlTableFunctionQuery(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="DataTable"/></returns>
         DataTable SqlTableFunctionQuery(string name, params object[] parameters);
+        #endregion
 
+        #region 执行表值函数 返回 DataTable + Task<DataTable> SqlTableFunctionQueryAsync(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         Task<DataTable> SqlTableFunctionQueryAsync(string name, params object[] parameters);
+        #endregion
 
+        #region 执行表值函数 返回 DataTable + DataTable SqlTableFunctionQuery(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">函数模型</param>
+        /// <returns><see cref="DataTable"/></returns>
         DataTable SqlTableFunctionQuery(string name, object parameterModel);
+        #endregion
 
+        #region 执行表值函数 返回 DataTable + Task<DataTable> SqlTableFunctionQueryAsync(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         Task<DataTable> SqlTableFunctionQueryAsync(string name, object parameterModel);
+        #endregion
 
+
+        #region 执行表值函数 + IEnumerable<T> SqlTableFunctionQuery<T>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
         IEnumerable<T> SqlTableFunctionQuery<T>(string name, params object[] parameters);
+        #endregion
 
+        #region 执行表值函数 + Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, params object[] parameters);
+        #endregion
 
+        #region 执行表值函数 + IEnumerable<T> SqlTableFunctionQuery<T>(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
         IEnumerable<T> SqlTableFunctionQuery<T>(string name, object parameterModel);
+        #endregion
 
+        #region 执行表值函数 + Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, object parameterModel);
+        #endregion
     }
 }
--- aspnetcore/Fur/DatabaseVisitor/Repositories/Partials/implements/SqlFunctionPartial.cs
+++ aspnetcore/Fur/DatabaseVisitor/Repositories/Partials/implements/SqlFunctionPartial.cs
@@ -15,67 +15,172 @@ namespace Fur.DatabaseVisitor.Repositories
     /// <typeparam name="TEntity">实体类型</typeparam>
     public partial class EFCoreRepositoryOfT<TEntity> : IRepositoryOfT<TEntity>, IScopedLifetimeOfT<TEntity> where TEntity : class, IDbEntity, new()
     {
-        // 标量函数
-        public virtual TResult SqlScalarFunctionQuery<TResult>(string name, params object[] parameters) where TResult : struct
+        #region 执行标量函数 + public virtual TResult SqlScalarFunctionQuery<TResult>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns>结果类型值</returns>
+        public virtual TResult SqlScalarFunctionQuery<TResult>(string name, params object[] parameters)
         {
             return Database.SqlFunctionExecute<TResult>(DbFunctionTypeOptions.Scalar, name, parameters).FirstOrDefault();
         }
+        #endregion
 
-        public virtual async Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, params object[] parameters) where TResult : struct
+        #region 执行标量函数 + public virtual async Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
+        public virtual async Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, params object[] parameters)
         {
             var results = await Database.SqlFunctionExecuteAsync<TResult>(DbFunctionTypeOptions.Scalar, name, parameters);
             return results.FirstOrDefault();
         }
+        #endregion
 
-        public virtual TResult SqlScalarFunctionQuery<TResult>(string name, object parameterModel) where TResult : struct
+        #region 执行标量函数 + public virtual TResult SqlScalarFunctionQuery<TResult>(string name, object parameterModel)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns>结果类型值</returns>
+        public virtual TResult SqlScalarFunctionQuery<TResult>(string name, object parameterModel)
         {
             return Database.SqlFunctionExecute<TResult>(DbFunctionTypeOptions.Scalar, name, parameterModel).FirstOrDefault();
         }
+        #endregion
 
-        public virtual async Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, object parameterModel) where TResult : struct
+        #region 执行标量函数 + public virtual async Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, object parameterModel)
+        /// <summary>
+        /// 执行标量函数
+        /// </summary>
+        /// <typeparam name="TResult">结果类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
+        public virtual async Task<TResult> SqlScalarFunctionQueryAsync<TResult>(string name, object parameterModel)
         {
             var results = await Database.SqlFunctionExecuteAsync<TResult>(DbFunctionTypeOptions.Scalar, name, parameterModel);
             return results.FirstOrDefault();
         }
+        #endregion
 
+
+        #region 执行表值函数 返回 DataTable + public virtual DataTable SqlTableFunctionQuery(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="DataTable"/></returns>
         public virtual DataTable SqlTableFunctionQuery(string name, params object[] parameters)
         {
             return Database.SqlFunctionExecute(DbFunctionTypeOptions.Table, name, parameters);
         }
+        #endregion
 
+        #region 执行表值函数 返回 DataTable + public virtual Task<DataTable> SqlTableFunctionQueryAsync(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         public virtual Task<DataTable> SqlTableFunctionQueryAsync(string name, params object[] parameters)
         {
             return Database.SqlFunctionExecuteAsync(DbFunctionTypeOptions.Table, name, parameters);
         }
+        #endregion
 
+        #region 执行表值函数 返回 DataTable + public virtual DataTable SqlTableFunctionQuery(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">函数模型</param>
+        /// <returns><see cref="DataTable"/></returns>
         public virtual DataTable SqlTableFunctionQuery(string name, object parameterModel)
         {
             return Database.SqlFunctionExecute(DbFunctionTypeOptions.Table, name, parameterModel);
         }
+        #endregion
 
+        #region 执行表值函数 返回 DataTable + public virtual Task<DataTable> SqlTableFunctionQueryAsync(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数 返回 DataTable
+        /// </summary>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         public virtual Task<DataTable> SqlTableFunctionQueryAsync(string name, object parameterModel)
         {
             return Database.SqlFunctionExecuteAsync(DbFunctionTypeOptions.Table, name, parameterModel);
         }
+        #endregion
 
+
+        #region 执行表值函数 + public virtual IEnumerable<T> SqlTableFunctionQuery<T>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
         public virtual IEnumerable<T> SqlTableFunctionQuery<T>(string name, params object[] parameters)
         {
             return Database.SqlFunctionExecute<T>(DbFunctionTypeOptions.Table, name, parameters);
         }
+        #endregion
 
+        #region 执行表值函数 + public virtual Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, params object[] parameters)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameters"><see cref="SqlParameter"/> 参数</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         public virtual Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, params object[] parameters)
         {
             return Database.SqlFunctionExecuteAsync<T>(DbFunctionTypeOptions.Table, name, parameters);
         }
+        #endregion
 
+        #region 执行表值函数 + public virtual IEnumerable<T> SqlTableFunctionQuery<T>(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
         public virtual IEnumerable<T> SqlTableFunctionQuery<T>(string name, object parameterModel)
         {
             return Database.SqlFunctionExecute<T>(DbFunctionTypeOptions.Table, name, parameterModel);
         }
+        #endregion
 
+        #region 执行表值函数 + public virtual Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, object parameterModel)
+        /// <summary>
+        /// 执行表值函数
+        /// </summary>
+        /// <typeparam name="T">结果集类型</typeparam>
+        /// <param name="name">函数名称</param>
+        /// <param name="parameterModel">参数模型</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
         public virtual Task<IEnumerable<T>> SqlTableFunctionQueryAsync<T>(string name, object parameterModel)
         {
             return Database.SqlFunctionExecuteAsync<T>(DbFunctionTypeOptions.Table, name, parameterModel);
         }
+        #endregion
     }
 }
