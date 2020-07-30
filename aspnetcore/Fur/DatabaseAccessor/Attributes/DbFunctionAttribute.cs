b44cad0443cbd0a713b466c02a047f5c95492f7c
Monk <monksoul@outlook.com>
2020-07-30 14:06:45 +0800

    link #I1OT9N

--- aspnetcore/Fur/DatabaseAccessor/Attributes/DbFunctionAttribute.cs
+++ aspnetcore/Fur/DatabaseAccessor/Attributes/DbFunctionAttribute.cs
@@ -4,10 +4,12 @@ using System;
 namespace Fur.DatabaseAccessor.Attributes
 {
     /// <summary>
-    /// 数据库函数定位器
-    /// <para>覆盖 <see cref="Microsoft.EntityFrameworkCore.DbFunctionAttribute"/>，提供数据库上下文定位器功能</para>
-    /// <para>说明：只对静态类中的静态方法起作用</para>
+    /// Linq中使用数据库函数特性类
     /// </summary>
+    /// <remarks>
+    /// <para>覆盖 <see cref="Microsoft.EntityFrameworkCore.DbFunctionAttribute"/>，提供数据库上下文定位器功能</para>
+    /// <para>说明：该特性只对静态类中的静态方法有效</para>
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Method), NonWrapper]
     public class DbFunctionAttribute : Microsoft.EntityFrameworkCore.DbFunctionAttribute
     {
@@ -15,23 +17,25 @@ namespace Fur.DatabaseAccessor.Attributes
         /// 构造函数
         /// </summary>
         /// <param name="name">函数名</param>
-        /// <param name="schema">函数schema</param>
+        /// <param name="schema">数据库架构名</param>
         public DbFunctionAttribute(string name, string schema)
             : base(name, schema) { }
 
         /// <summary>
         /// 构造函数
-        /// <para>可以指定数据库上下文定位器类型</para>
         /// </summary>
+        /// <remarks>
+        /// <para>可以指定数据库上下文定位器类型</para>
+        /// </remarks>
         /// <param name="name">函数名</param>
-        /// <param name="schema">函数schema</param>
-        /// <param name="dbContextLocators">数据库上下文定位器类型</param>
+        /// <param name="schema">数据库架构名</param>
+        /// <param name="dbContextLocators">数据库上下文定位器</param>
         public DbFunctionAttribute(string name, string schema, params Type[] dbContextLocators)
             : base(name, schema)
             => DbContextLocators = dbContextLocators;
 
         /// <summary>
-        /// 数据库上下文定位器类型
+        /// 数据库上下文定位器
         /// </summary>
         public Type[] DbContextLocators { get; set; }
     }
