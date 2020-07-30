0e3345bd5d31df8744671ba074711c63bbfe10b6
Monk <monksoul@outlook.com>
2020-07-30 14:51:48 +0800

    link #I1OT9N

--- aspnetcore/Fur/DatabaseAccessor/Contexts/Pools/DbContextPool.cs
+++ aspnetcore/Fur/DatabaseAccessor/Contexts/Pools/DbContextPool.cs
@@ -8,15 +8,20 @@ namespace Fur.DatabaseAccessor.Contexts.Pools
 {
     /// <summary>
     /// 数据库上下文池
-    /// <para>用来管理请求中所有创建的 <see cref="DbContext"/> 对象</para>
-    /// <para>说明：非依赖注入方式创建的 <see cref="DbContext"/> 需手动调用 <see cref="SaveDbContext(DbContext)"/> 或 <see cref="SaveDbContextAsync(DbContext)"/> 保存到数据库上下文池中</para>
-    /// <para>数据库上下文池必须注册为 <c>Scope</c> 范围实例，保证单次请求唯一，参见：依赖注入章节：<see cref="https://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-3.1"/></para>
     /// </summary>
-    public class DbContextPool : IDbContextPool
+    /// <remarks>
+    /// <para>用来管理请求中所有创建的 <see cref="DbContext"/> 实例</para>
+    /// <para>非依赖注入方式创建的 <see cref="DbContext"/> 需手动调用 <see cref="SaveDbContext(DbContext)"/> 或 <see cref="SaveDbContextAsync(DbContext)"/> 保存到数据库上下文池中</para>
+    /// <para>数据库上下文池必须注册为 <c>Scope</c> 范围实例，保证单次请求唯一，参见：依赖注入章节：<see cref="https://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-3.1"/></para>
+    /// </remarks>
+    public sealed class DbContextPool : IDbContextPool
     {
         /// <summary>
         /// 线程安全的数据库上下文集合
         /// </summary>
+        /// <remarks>
+        /// <para>避免重复追加数据库上下文到数据库上下文池中</para>
+        /// </remarks>
         private readonly ConcurrentBag<DbContext> dbContexts;
 
         /// <summary>
@@ -57,11 +62,9 @@ namespace Fur.DatabaseAccessor.Contexts.Pools
         /// <summary>
         /// 获取数据库上下文池中所有数据库上下文
         /// </summary>
-        /// <returns><see cref="IEnumerable{T}"/></returns>
+        /// <returns>数据库上下文池中所有数据库上下文集合</returns>
         public IEnumerable<DbContext> GetDbContexts()
-        {
-            return dbContexts;
-        }
+            => dbContexts;
 
         /// <summary>
         /// 提交数据库上下文池中所有已更改的数据库上下文
--- aspnetcore/Fur/DatabaseAccessor/Contexts/Pools/IDbContextPool.cs
+++ aspnetcore/Fur/DatabaseAccessor/Contexts/Pools/IDbContextPool.cs
@@ -6,10 +6,12 @@ namespace Fur.DatabaseAccessor.Contexts.Pools
 {
     /// <summary>
     /// 数据库上下文池
-    /// <para>用来管理请求中所有创建的 <see cref="DbContext"/> 对象</para>
-    /// <para>说明：非依赖注入方式创建的 <see cref="DbContext"/> 需手动调用 <see cref="SaveDbContext(DbContext)"/> 或 <see cref="SaveDbContextAsync(DbContext)"/> 保存到数据库上下文池中</para>
-    /// <para>数据库上下文池必须注册为 <c>Scope</c> 范围实例，保证单次请求唯一，参见：依赖注入章节：<see cref="https://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-3.1"/></para>
     /// </summary>
+    /// <remarks>
+    /// <para>用来管理请求中所有 <see cref="DbContext"/> 实例</para>
+    /// <para>非依赖注入方式创建的 <see cref="DbContext"/> 需手动调用 <see cref="SaveDbContext(DbContext)"/> 或 <see cref="SaveDbContextAsync(DbContext)"/> 保存到数据库上下文池中</para>
+    /// <para>数据库上下文池必须注册为 <c>Scope</c> 范围实例，保证单次请求唯一，参见：依赖注入章节：<see cref="https://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-3.1"/></para>
+    /// </remarks>
     public interface IDbContextPool
     {
         /// <summary>
@@ -28,7 +30,7 @@ namespace Fur.DatabaseAccessor.Contexts.Pools
         /// <summary>
         /// 获取数据库上下文池中所有数据库上下文
         /// </summary>
-        /// <returns><see cref="IEnumerable{T}"/></returns>
+        /// <returns>数据库上下文池中所有数据库上下文集合</returns>
         IEnumerable<DbContext> GetDbContexts();
 
         /// <summary>
