0be717044fb016662a5fac7763f442071a32ca99
Monk <monksoul@outlook.com>
2020-08-04 13:50:55 +0800

    link #I1Q35H

---
+++ aspnetcore/Fur/FriendlyException/Attributes/IfExceptionAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Fur.AppCore.Attributes;
+using System;
+
+namespace Fur.FriendlyException.Attributes
+{
+    /// <summary>
+    /// 支持复写默认异常提供器
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true), NonInflated]
+    public sealed class IfExceptionAttribute : Attribute
+    {
+        public IfExceptionAttribute(int exceptionCode, string message)
+        {
+            ExceptionCode = exceptionCode;
+            Message = message;
+        }
+
+        /// <summary>
+        /// 异常代码
+        /// </summary>
+        public int ExceptionCode { get; set; }
+
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
