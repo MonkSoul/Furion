9b48d0e18aed5978d0530b4a4d586b03952dad77
Monk <monksoul@outlook.com>
2020-08-04 09:55:54 +0800

    update documents.

--- aspnetcore/Fur/AppCore/Attributes/NonInflatedAttribute.cs
+++ aspnetcore/Fur/AppCore/Attributes/NonInflatedAttribute.cs
@@ -3,12 +3,8 @@
 namespace Fur.AppCore.Attributes
 {
     /// <summary>
-    /// 禁用被应用启动时包装类型、方法、属性
+    /// 被贴此特性的类型、属性、方法都将在应用启动时被忽略装载包装
     /// </summary>
-    /// <remarks>
-    /// <para>应用会在启动时扫描所有程序集、类型、方法、属性进行包装，以在运行时快速查找</para>
-    /// <para>但排除贴了 <see cref="NonInflatedAttribute"/> 特性的所有类型</para>
-    /// </remarks>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property), NonInflated]
     public sealed class NonInflatedAttribute : Attribute { }
 }
