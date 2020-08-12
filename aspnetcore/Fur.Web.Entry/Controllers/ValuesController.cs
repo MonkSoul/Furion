cb4393157f5a6a1479f0f0e2cd5b9836af5839b3
Monk <monksoul@outlook.com>
2020-08-12 19:18:43 +0800

    link #I1QYAI

--- aspnetcore/Fur.Web.Entry/Controllers/ValuesController.cs
+++ aspnetcore/Fur.Web.Entry/Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace Fur.Web.Entry.Controllers
     {
         [HttpGet]
         [HttpGet("dfd")]
+        [AcceptVerbs("POST")]
         public string Get()
         {
             return "1";
