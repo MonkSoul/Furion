0ce9b62134250f83b9142c761a72b746ebea4474
Monk <monksoul@outlook.com>
2020-08-13 16:37:51 +0800

    update documents.

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,132 @@
+module.exports = {
+  title: "Fur",
+  tagline: "小僧不才，略懂皮毛（Fur）。",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Fur",
+      logo: {
+        alt: "Fur Logo",
+        src: "img/logo.svg",
+      },
+      hideOnScroll: true,
+      items: [
+        {
+          type: "docsVersionDropdown",
+          position: "left",
+        },
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "手册",
+          position: "left",
+        },
+        { to: "blog", label: "博客", position: "left" },
+        { label: "配置", position: "left" },
+        { label: "视频", position: "right" },
+        {
+          label: "工具",
+          position: "right",
+          items: [
+            {
+              label: "代码生成器",
+            },
+          ],
+        },
+        {
+          href: "https://gitee.com/monksoul/Fur/board",
+          label: "看板",
+          position: "right",
+        },
+        {
+          label: "仓库",
+          position: "right",
+          items: [
+            {
+              label: "Gitee",
+              href: "https://gitee.com/monksoul/Fur",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/MonkSoul/Fur",
+            },
+          ],
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "手册",
+          items: [
+            {
+              label: "快速入门",
+              to: "docs/",
+            },
+            {
+              label: "文档指南",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "社区",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "更多",
+          items: [
+            {
+              label: "博客",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Fur, Baiqian. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: "doc1",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
---
+++ docs/sidebars.js
@@ -0,0 +1,6 @@
+module.exports = {
+  docs: {
+    选项与配置: ["doc1", "doc2", "doc3"],
+    数据库操作: ["mdx"],
+  },
+};
