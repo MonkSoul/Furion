74e620ce0fcc2a78393f2902481934f748eda778
Monk <monksoul@outlook.com>
2020-08-20 11:41:05 +0800

    update documents.

--- aspnetcore/Fur.Application/FurAppService.cs
+++ aspnetcore/Fur.Application/FurAppService.cs
@@ -1,24 +1,14 @@
 ﻿using Fur.DynamicApiController;
-using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fur.Application
 {
     public class FurAppService : IDynamicApiController
     {
-        public string RouteSeat(int id, string name)
+        [HttpPost, HttpGet, AcceptVerbs("PUT", "DELETE")]
+        public string GetVersion()
         {
-            return "配置路由参数位置";
-        }
-
-        public string RouteSeat(
-            [ApiSeat(ApiSeats.ControllerStart)] int id, // 控制器名称之前
-            [ApiSeat(ApiSeats.ControllerEnd)] string name, // 控制器名称之后
-            [ApiSeat(ApiSeats.ControllerEnd)] int age, // 控制器名称之后
-            [ApiSeat(ApiSeats.ActionStart)] decimal weight, // 动作方法名称之前
-            [ApiSeat(ApiSeats.ActionStart)] float height, // 动作方法名称之前
-            [ApiSeat(ApiSeats.ActionEnd)] DateTime birthday) // 动作方法名称之后（默认值）
-        {
-            return "配置路由参数位置";
+            return "1.0.0";
         }
     }
 }
