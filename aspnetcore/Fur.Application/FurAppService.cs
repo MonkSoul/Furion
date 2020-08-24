dcd2ff12a00e7866693652d002c2bb25578f85f7
Monk <monksoul@outlook.com>
2020-08-24 18:02:39 +0800

    update documents.

--- aspnetcore/Fur.Application/FurAppService.cs
+++ aspnetcore/Fur.Application/FurAppService.cs
@@ -2,25 +2,28 @@
 
 namespace Fur.Application
 {
-    /// <summary>
-    /// 框架接口
-    /// </summary>
+    [ApiDescriptionSettings(Tag = "合并所有标签")]
     public class FurAppService : IDynamicApiController
     {
-        /// <summary>
-        /// 动作方法注释
-        /// </summary>
-        /// <returns></returns>
         public string Get()
         {
             return nameof(Fur);
         }
 
-        /// <summary>
-        /// 通过Id获取
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        public int Get(int id)
+        {
+            return id;
+        }
+    }
+
+    [ApiDescriptionSettings(Tag = "合并所有标签")]
+    public class TestAppService : IDynamicApiController
+    {
+        public string Get()
+        {
+            return nameof(Fur);
+        }
+
         public int Get(int id)
         {
             return id;
