8fd079faa8d551f5e5ae336eedd9893139b160ec
Monk <monksoul@outlook.com>
2020-08-24 18:04:56 +0800

    update documents.

--- aspnetcore/Fur.Application/FurAppService.cs
+++ aspnetcore/Fur.Application/FurAppService.cs
@@ -2,22 +2,7 @@
 
 namespace Fur.Application
 {
-    [ApiDescriptionSettings(Tag = "合并所有标签")]
     public class FurAppService : IDynamicApiController
-    {
-        public string Get()
-        {
-            return nameof(Fur);
-        }
-
-        public int Get(int id)
-        {
-            return id;
-        }
-    }
-
-    [ApiDescriptionSettings(Tag = "合并所有标签")]
-    public class TestAppService : IDynamicApiController
     {
         public string Get()
         {
