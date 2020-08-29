9ba41621bacac05efa54d00f84eb5e22ee477f32
Monk <monksoul@outlook.com>
2020-08-29 10:53:25 +0800

    update documents.

--- aspnetcore/Fur.Application/FurAppService.cs
+++ aspnetcore/Fur.Application/FurAppService.cs
@@ -1,23 +1,14 @@
 ﻿using Fur.DataValidation;
 using Fur.DynamicApiController;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace Fur.Application
 {
     public class FurAppService : IDynamicApiController
     {
-        public int Get(int id)
+        [NonVaildate] // 跳过全局验证
+        public DataValidationResult Post(TestDto testDto)
         {
-            return id;
-        }
-
-        public List<ValidationResult> Post(TestDto testDto)
-        {
-            var validate = "".TryValidate(ValidationTypes.Required);
-
-            return validate.ValidationResults.ToList();
+            return testDto.TryValidate();
         }
     }
 }
--- aspnetcore/Fur.Application/TestDto.cs
+++ aspnetcore/Fur.Application/TestDto.cs
@@ -1,12 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Fur.Application
 {
-    public class TestDto
+    public class TestDto : IValidatableObject
     {
+        [Range(10, 20, ErrorMessage = "Id 只能在 10-20 区间取值")]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "必填"), MinLength(3, ErrorMessage = "字符串长度不能少于3位")]
         public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Name.StartsWith("Fur"))
+            {
+                yield return new ValidationResult(
+                    "不能以 Fur 开头"
+                    , new[] { nameof(Name) }
+                );
+            }
+        }
     }
 }
