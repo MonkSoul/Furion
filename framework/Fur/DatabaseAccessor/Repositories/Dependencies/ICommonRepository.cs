3181b6eb7b6b0d38992760b2c86611f1c385e8da
Monk <monksoul@outlook.com>
2020-09-01 18:37:25 +0800

    link #I1T8EC

---
+++ framework/Fur/DatabaseAccessor/Repositories/Dependencies/ICommonRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Fur.DatabaseAccessor
+{
+    /// <summary>
+    /// 公共仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public partial interface ICommonRepository<TEntity>
+       where TEntity : class, IDbEntityBase, new()
+    {
+    }
+}
