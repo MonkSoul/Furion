355479b5907cdd8b89b4b9a550d6e4572db767b2
Monk <monksoul@outlook.com>
2020-09-03 16:41:47 +0800

    link #I1LT5R

---
+++ framework/Fur/DatabaseAccessor/Repositories/Dependencies/ISqlExecutableRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Fur.DatabaseAccessor
+{
+    /// <summary>
+    /// Sql 执行仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface ISqlExecutableRepository<TEntity>
+        where TEntity : class, IEntityBase, new()
+    {
+    }
+}
---
+++ framework/Fur/DatabaseAccessor/Repositories/Dependencies/ISqlQueryableRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Fur.DatabaseAccessor
+{
+    /// <summary>
+    /// Sql 查询仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface ISqlQueryableRepository<TEntity>
+        where TEntity : class, IEntityBase, new()
+    {
+    }
+}
