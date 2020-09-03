11c1f406c2468282773898b76791a79a038c77b7
Monk <monksoul@outlook.com>
2020-09-03 15:35:29 +0800

    link #I1T8EC

---
+++ framework/Fur/DatabaseAccessor/Repositories/Implantations/OperableRepository.cs
@@ -0,0 +1,18 @@
+﻿// 框架名称：Fur
+// 框架作者：百小僧
+// 框架版本：1.0.0
+// 开源协议：MIT
+// 项目地址：https://gitee.com/monksoul/Fur
+
+namespace Fur.DatabaseAccessor
+{
+    /// <summary>
+    /// 可操作仓储分部类
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    public partial class EFCoreRepository<TEntity>
+         where TEntity : class, IEntityBase, new()
+    {
+
+    }
+}
