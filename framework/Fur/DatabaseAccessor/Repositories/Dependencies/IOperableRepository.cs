da83ec73581a53f3303d49217b4a3b56f0e71a30
Monk <monksoul@outlook.com>
2020-09-03 15:51:34 +0800

    link #I1T8EC

--- framework/Fur/DatabaseAccessor/Repositories/Dependencies/IOperableRepository.cs
+++ framework/Fur/DatabaseAccessor/Repositories/Dependencies/IOperableRepository.cs
@@ -4,6 +4,10 @@
 // 开源协议：MIT
 // 项目地址：https://gitee.com/monksoul/Fur
 
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Fur.DatabaseAccessor
 {
     /// <summary>
@@ -13,5 +17,51 @@ namespace Fur.DatabaseAccessor
     public partial interface IOperableRepository<TEntity>
         where TEntity : class, IEntityBase, new()
     {
+        /// <summary>
+        /// 新增或更新一条记录
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns>代理中的实体</returns>
+        EntityEntry<TEntity> InsertOrUpdate(TEntity entity);
+
+        /// <summary>
+        /// 新增或更新一条记录
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>代理中的实体</returns>
+        Task<EntityEntry<TEntity>> InsertOrUpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns>数据库中的实体</returns>
+        EntityEntry<TEntity> InsertOrUpdateNow(TEntity entity);
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="acceptAllChangesOnSuccess">接受所有更改</param>
+        /// <returns>数据库中的实体</returns>
+        EntityEntry<TEntity> InsertOrUpdateNow(TEntity entity, bool acceptAllChangesOnSuccess);
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>数据库中的实体</returns>
+        Task<EntityEntry<TEntity>> InsertOrUpdateNowAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="acceptAllChangesOnSuccess">接受所有更改</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>数据库中的实体</returns>
+        Task<EntityEntry<TEntity>> InsertOrUpdateNowAsync(TEntity entity, bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
     }
 }
--- framework/Fur/DatabaseAccessor/Repositories/Implantations/OperableRepository.cs
+++ framework/Fur/DatabaseAccessor/Repositories/Implantations/OperableRepository.cs
@@ -4,6 +4,10 @@
 // 开源协议：MIT
 // 项目地址：https://gitee.com/monksoul/Fur
 
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Fur.DatabaseAccessor
 {
     /// <summary>
@@ -13,6 +17,69 @@ namespace Fur.DatabaseAccessor
     public partial class EFCoreRepository<TEntity>
          where TEntity : class, IEntityBase, new()
     {
+        /// <summary>
+        /// 新增或更新一条记录
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns>代理中的实体</returns>
+        public virtual EntityEntry<TEntity> InsertOrUpdate(TEntity entity)
+        {
+            return IsKeySet(entity) ? Update(entity) : Insert(entity);
+        }
 
+        /// <summary>
+        /// 新增或更新一条记录
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>代理中的实体</returns>
+        public virtual Task<EntityEntry<TEntity>> InsertOrUpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            return IsKeySet(entity) ? UpdateAsync(entity) : InsertAsync(entity, cancellationToken);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns>数据库中的实体</returns>
+        public virtual EntityEntry<TEntity> InsertOrUpdateNow(TEntity entity)
+        {
+            return IsKeySet(entity) ? UpdateNow(entity) : InsertNow(entity);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="acceptAllChangesOnSuccess">接受所有更改</param>
+        /// <returns>数据库中的实体</returns>
+        public virtual EntityEntry<TEntity> InsertOrUpdateNow(TEntity entity, bool acceptAllChangesOnSuccess)
+        {
+            return IsKeySet(entity) ? UpdateNow(entity, acceptAllChangesOnSuccess) : InsertNow(entity, acceptAllChangesOnSuccess);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>数据库中的实体</returns>
+        public virtual Task<EntityEntry<TEntity>> InsertOrUpdateNowAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            return IsKeySet(entity) ? UpdateNowAsync(entity, cancellationToken) : InsertNowAsync(entity, cancellationToken);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="acceptAllChangesOnSuccess">接受所有更改</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>数据库中的实体</returns>
+        public virtual Task<EntityEntry<TEntity>> InsertOrUpdateNowAsync(TEntity entity, bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            return IsKeySet(entity) ? UpdateNowAsync(entity, acceptAllChangesOnSuccess, cancellationToken) : InsertNowAsync(entity, acceptAllChangesOnSuccess, cancellationToken);
+        }
     }
 }
