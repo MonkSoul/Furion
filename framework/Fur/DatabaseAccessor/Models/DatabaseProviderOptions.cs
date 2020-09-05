3e1f580f01d27e554963bf9590ef079d467ced5a
百小僧 <MonkSoul@outlook.com>
2020-09-05 20:03:21 +0800

    🤯 update licence to Apache-2.0

--- framework/Fur/DatabaseAccessor/Models/DatabaseProviderOptions.cs
+++ framework/Fur/DatabaseAccessor/Models/DatabaseProviderOptions.cs
@@ -1,4 +1,10 @@
-﻿namespace Fur.DatabaseAccessor
+﻿// 框架名称：Fur
+// 框架作者：百小僧
+// 框架版本：1.0.0
+// 开源协议：Apache-2.0
+// 项目地址：https://gitee.com/monksoul/Fur
+
+namespace Fur.DatabaseAccessor
 {
     /// <summary>
     /// 数据库提供器选项
