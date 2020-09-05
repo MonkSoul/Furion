a5a40cf800a9b9912c9400087856193c5517f3ac
百小僧 <MonkSoul@outlook.com>
2020-09-05 23:30:46 +0800

    🤴 update LICENSE

--- framework/Fur/App/App.cs
+++ framework/Fur/App/App.cs
@@ -5,8 +5,8 @@
 // 框架名称：Fur
 // 框架作者：百小僧
 // 框架版本：1.0.0
-// 源码地址：https://gitee.com/monksoul/Fur
-// 开源协议：Apache-2.0（https://gitee.com/monksoul/Fur/blob/alpha/LICENSE）
+// 源码地址：https://gitee.com/monksoul/Fur 
+// 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
 // --------------------------------------------------------------------------------------
 
 using Fur.ConfigurableOptions;
--- framework/Fur/Authorization/Requirements/AuthorizePolicyRequirement.cs
+++ framework/Fur/Authorization/Requirements/AuthorizePolicyRequirement.cs
@@ -6,7 +6,7 @@
 // 框架作者：百小僧
 // 框架版本：1.0.0
 // 源码地址：https://gitee.com/monksoul/Fur 
-// 开源协议：Apache-2.0（https://gitee.com/monksoul/Fur/blob/alpha/LICENSE）
+// 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
 // --------------------------------------------------------------------------------------
 
 using Microsoft.AspNetCore.Authorization;
