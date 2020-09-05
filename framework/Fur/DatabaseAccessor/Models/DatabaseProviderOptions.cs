cf54a5c10d7c7a170e454bbfff560a99787b684a
百小僧 <MonkSoul@outlook.com>
2020-09-05 20:10:58 +0800

    😈 update file header

--- framework/Fur/DatabaseAccessor/Models/DatabaseProviderOptions.cs
+++ framework/Fur/DatabaseAccessor/Models/DatabaseProviderOptions.cs
@@ -1,8 +1,8 @@
 ﻿// 框架名称：Fur
 // 框架作者：百小僧
 // 框架版本：1.0.0
-// 开源协议：Apache-2.0
-// 项目地址：https://gitee.com/monksoul/Fur
+// 源码地址：https://gitee.com/monksoul/Fur 
+// 开源协议：Apache-2.0（https://gitee.com/monksoul/Fur/blob/alpha/LICENSE）
 
 namespace Fur.DatabaseAccessor
 {
