9235eb79127e9cec7e3540c0498114f10a9633c1
百小僧 <MonkSoul@outlook.com>
2020-09-05 20:17:56 +0800

    🧔 update file header

--- framework/Fur/FriendlyException/Oops.cs
+++ framework/Fur/FriendlyException/Oops.cs
@@ -1,8 +1,13 @@
-﻿// 框架名称：Fur
+﻿// --------------------------------------------------------------------------------------
+// Fur 是 .NET 5 平台下极易入门、极速开发的 Web 应用框架。
+// Copyright © 2020 Fur, Baiqian Co.,Ltd.
+//
+// 框架名称：Fur
 // 框架作者：百小僧
 // 框架版本：1.0.0
 // 源码地址：https://gitee.com/monksoul/Fur 
 // 开源协议：Apache-2.0（https://gitee.com/monksoul/Fur/blob/alpha/LICENSE）
+// --------------------------------------------------------------------------------------
 
 using Fur.DynamicApiController;
 using Fur.Extensions;
