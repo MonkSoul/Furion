89066395d7b213c77caddd884953901c35809a28
Gitea <gitea@fake.local>
2020-09-13 00:21:32 +0800

    🌌 add IEntityExtensions InsertOrUpdate functions. (#I1T8EC)

--- framework/Fur/DatabaseAccessor/Extensions/Repositories/IEntityExtensions.cs
+++ framework/Fur/DatabaseAccessor/Extensions/Repositories/IEntityExtensions.cs
@@ -890,6 +890,77 @@ namespace Fur.DatabaseAccessor
         }
 
         /// <summary>
+        /// 新增或更新一条记录
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns>代理中的实体</returns>
+        public static EntityEntry<TEntity> InsertOrUpdate<TEntity>(this TEntity entity)
+            where TEntity : class, IEntity, new()
+        {
+            return GetRepository<TEntity>().InsertOrUpdate(entity);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>代理中的实体</returns>
+        public static Task<EntityEntry<TEntity>> InsertOrUpdateAsync<TEntity>(this TEntity entity, CancellationToken cancellationToken = default)
+            where TEntity : class, IEntity, new()
+        {
+            return GetRepository<TEntity>().InsertOrUpdateAsync(entity, cancellationToken);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns>数据库中的实体</returns>
+        public static EntityEntry<TEntity> InsertOrUpdateNow<TEntity>(this TEntity entity)
+            where TEntity : class, IEntity, new()
+        {
+            return GetRepository<TEntity>().InsertOrUpdateNow(entity);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="acceptAllChangesOnSuccess">接受所有更改</param>
+        /// <returns>数据库中的实体</returns>
+        public static EntityEntry<TEntity> InsertOrUpdateNow<TEntity>(this TEntity entity, bool acceptAllChangesOnSuccess)
+            where TEntity : class, IEntity, new()
+        {
+            return GetRepository<TEntity>().InsertOrUpdateNow(entity, acceptAllChangesOnSuccess);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>数据库中的实体</returns>
+        public static Task<EntityEntry<TEntity>> InsertOrUpdateNowAsync<TEntity>(this TEntity entity, CancellationToken cancellationToken = default)
+            where TEntity : class, IEntity, new()
+        {
+            return GetRepository<TEntity>().InsertOrUpdateNowAsync(entity, cancellationToken);
+        }
+
+        /// <summary>
+        /// 新增或更新一条记录并立即执行
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="acceptAllChangesOnSuccess">接受所有更改</param>
+        /// <param name="cancellationToken">取消异步令牌</param>
+        /// <returns>数据库中的实体</returns>
+        public static Task<EntityEntry<TEntity>> InsertOrUpdateNowAsync<TEntity>(this TEntity entity, bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+            where TEntity : class, IEntity, new()
+        {
+            return GetRepository<TEntity>().InsertOrUpdateNowAsync(entity, acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        /// <summary>
         /// 获取实体仓储
         /// </summary>
         /// <typeparam name="TEntity">实体类型</typeparam>
