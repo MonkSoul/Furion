57b81ce6e68d660dc02b67527be40c287d69df4f
Monk <monksoul@outlook.com>
2020-09-16 09:36:58 +0800

    😮 completed ObjectMapper.

---
+++ framework/Fur/ObjectMapper/Extensions/ObjectMapperServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------------
+// Fur 是 .NET 5 平台下极易入门、极速开发的 Web 应用框架。
+// Copyright © 2020 Fur, Baiqian Co.,Ltd.
+//
+// 框架名称：Fur
+// 框架作者：百小僧
+// 框架版本：1.0.0
+// 源码地址：https://gitee.com/monksoul/Fur
+// 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
+// -----------------------------------------------------------------------------
+
+using Fur;
+using Fur.DependencyInjection;
+using Mapster;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// 对象映射拓展类
+    /// </summary>
+    [NonBeScan]
+    public static class ObjectMapperServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 添加对象映射
+        /// </summary>
+        /// <param name="services">服务集合</param>
+        /// <returns></returns>
+        public static IServiceCollection AddObjectMapper(this IServiceCollection services)
+        {
+            // 扫描所有继承  IRegister 接口的对象映射配置
+            TypeAdapterConfig.GlobalSettings.Scan(App.Assemblies.ToArray());
+
+            TypeAdapterConfig.GlobalSettings.Default
+                .NameMatchingStrategy(NameMatchingStrategy.IgnoreCase)
+                .PreserveReference(true);
+
+            return services;
+        }
+    }
+}
