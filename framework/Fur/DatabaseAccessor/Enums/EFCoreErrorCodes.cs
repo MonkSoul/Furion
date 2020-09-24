7785a5955b15eea3256bd8ae7a8166ba856ac2ed
Monk <monksoul@outlook.com>
2020-09-24 11:55:37 +0800

    🔞 update file header comments.

--- framework/Fur/DatabaseAccessor/Enums/EFCoreErrorCodes.cs
+++ framework/Fur/DatabaseAccessor/Enums/EFCoreErrorCodes.cs
@@ -5,7 +5,8 @@
 // 框架名称：Fur
 // 框架作者：百小僧
 // 框架版本：1.0.0
-// 源码地址：https://gitee.com/monksoul/Fur
+// 源码地址：Gitee：https://gitee.com/monksoul/Fur 
+// 				   Github：https://github.com/monksoul/Fur 
 // 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
 // -----------------------------------------------------------------------------
 
