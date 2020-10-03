f8c071d000a3eb72bf6eea8d86ea96739a13f566
百小僧 <monksoul@outlook.com>
2020-10-03 19:11:09 +0800

    🧻 add dynamic table and database supports.

---
+++ framework/Fur/DatabaseAccessor/DynamicModels/DynamicModelCacheKeyFactory.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------------
+// Fur 是 .NET 5 平台下极易入门、极速开发的 Web 应用框架。
+// Copyright © 2020 Fur, Baiqian Co.,Ltd.
+//
+// 框架名称：Fur
+// 框架作者：百小僧
+// 框架版本：1.0.0
+// 官方网站：https://chinadot.net
+// 源码地址：Gitee：https://gitee.com/monksoul/Fur
+// 				    Github：https://github.com/monksoul/Fur
+// 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
+// -----------------------------------------------------------------------------
+
+using Fur.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Threading;
+
+namespace Fur.DatabaseAccessor
+{
+    /// <summary>
+    /// 动态模型缓存工厂
+    /// </summary>
+    /// <remarks>主要用来实现数据库分表分库</remarks>
+    [SkipScan]
+    public class DynamicModelCacheKeyFactory : IModelCacheKeyFactory
+    {
+        /// <summary>
+        /// 动态模型缓存Key
+        /// </summary>
+        private static int cacheKey;
+
+        /// <summary>
+        /// 刷新实体类型
+        /// </summary>
+        /// <remarks>动态切换表之后需要调用该方法</remarks>
+        public static void RefreshEntityType()
+        {
+            Interlocked.Increment(ref cacheKey);
+        }
+
+        /// <summary>
+        /// 更新模型缓存
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public object Create(DbContext context)
+        {
+            return (context.GetType(), cacheKey);
+        }
+    }
+}
---
+++ framework/Fur/DatabaseAccessor/DynamicModels/IEntityMutableTable.cs
@@ -0,0 +1,155 @@
+﻿// -----------------------------------------------------------------------------
+// Fur 是 .NET 5 平台下极易入门、极速开发的 Web 应用框架。
+// Copyright © 2020 Fur, Baiqian Co.,Ltd.
+//
+// 框架名称：Fur
+// 框架作者：百小僧
+// 框架版本：1.0.0
+// 官方网站：https://chinadot.net
+// 源码地址：Gitee：https://gitee.com/monksoul/Fur
+// 				    Github：https://github.com/monksoul/Fur
+// 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
+// -----------------------------------------------------------------------------
+
+namespace Fur.DatabaseAccessor
+{
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator3">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2, TDbContextLocator3> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+        where TDbContextLocator3 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator3">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator4">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2, TDbContextLocator3, TDbContextLocator4> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+        where TDbContextLocator3 : class, IDbContextLocator
+        where TDbContextLocator4 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator3">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator4">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator5">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2, TDbContextLocator3, TDbContextLocator4, TDbContextLocator5> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+        where TDbContextLocator3 : class, IDbContextLocator
+        where TDbContextLocator4 : class, IDbContextLocator
+        where TDbContextLocator5 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator3">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator4">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator5">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator6">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2, TDbContextLocator3, TDbContextLocator4, TDbContextLocator5, TDbContextLocator6> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+        where TDbContextLocator3 : class, IDbContextLocator
+        where TDbContextLocator4 : class, IDbContextLocator
+        where TDbContextLocator5 : class, IDbContextLocator
+        where TDbContextLocator6 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator3">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator4">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator5">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator6">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator7">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2, TDbContextLocator3, TDbContextLocator4, TDbContextLocator5, TDbContextLocator6, TDbContextLocator7> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+        where TDbContextLocator3 : class, IDbContextLocator
+        where TDbContextLocator4 : class, IDbContextLocator
+        where TDbContextLocator5 : class, IDbContextLocator
+        where TDbContextLocator6 : class, IDbContextLocator
+        where TDbContextLocator7 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口
+    /// </summary>
+    /// <typeparam name="TDbContextLocator1">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator2">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator3">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator4">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator5">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator6">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator7">数据库上下文定位器</typeparam>
+    /// <typeparam name="TDbContextLocator8">数据库上下文定位器</typeparam>
+    public interface IEntityMutableTable<TDbContextLocator1, TDbContextLocator2, TDbContextLocator3, TDbContextLocator4, TDbContextLocator5, TDbContextLocator6, TDbContextLocator7, TDbContextLocator8> : IEntityMutableTableDependency
+        where TDbContextLocator1 : class, IDbContextLocator
+        where TDbContextLocator2 : class, IDbContextLocator
+        where TDbContextLocator3 : class, IDbContextLocator
+        where TDbContextLocator4 : class, IDbContextLocator
+        where TDbContextLocator5 : class, IDbContextLocator
+        where TDbContextLocator6 : class, IDbContextLocator
+        where TDbContextLocator7 : class, IDbContextLocator
+        where TDbContextLocator8 : class, IDbContextLocator
+    {
+    }
+
+    /// <summary>
+    /// 实体动态表依赖接口依赖（禁止外部继承）
+    /// </summary>
+    public interface IEntityMutableTableDependency
+    {
+        /// <summary>
+        /// 获取表名
+        /// </summary>
+        /// <returns></returns>
+        string GetTableName();
+    }
+}
