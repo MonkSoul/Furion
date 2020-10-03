53d0f35068e1a869e314e79ebf84794f2e9aabbc
百小僧 <monksoul@outlook.com>
2020-10-03 09:41:45 +0800

    🌩 update documents.

--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Fur",
   tagline: "Fur 是 .NET 5 平台下极易入门、极速开发的 Web 应用框架。",
   url: "https://chinadot.net",
-  baseUrl: "/",
+  baseUrl: "/fur/",
   onBrokenLinks: "throw",
   favicon: "img/favicon.ico",
   organizationName: "Baiqian Co.,Ltd.",
