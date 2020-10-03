a799193487f6365fb314a31156bf9d07d06d0193
百小僧 <monksoul@outlook.com>
2020-10-03 22:47:30 +0800

    🏒 adjust database provider extensions.

--- framework/Fur/DatabaseAccessor/Extensions/DatabaseProvider/SqlServerServiceCollectionExtensions.cs
+++ framework/Fur/DatabaseAccessor/Extensions/DatabaseProvider/SqlServerServiceCollectionExtensions.cs
@@ -15,6 +15,7 @@ using Fur.DatabaseAccessor;
 using Fur.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using System;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -32,16 +33,17 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="services">服务</param>
         /// <param name="connectionString">连接字符串</param>
         /// <param name="poolSize">池大小</param>
+        /// <param name="dynamicDbContext">动态数据库上下文，用于分表分库用</param>
         /// <param name="interceptors">拦截器</param>
         /// <returns>服务集合</returns>
-        public static IServiceCollection AddSqlServerPool<TDbContext>(this IServiceCollection services, string connectionString = default, int poolSize = 100, params IInterceptor[] interceptors)
+        public static IServiceCollection AddSqlServerPool<TDbContext>(this IServiceCollection services, string connectionString = default, int poolSize = 100, bool dynamicDbContext = false, params IInterceptor[] interceptors)
             where TDbContext : DbContext
         {
             // 避免重复注册默认数据库上下文
             if (Penetrates.DbContextWithLocatorCached.ContainsKey(typeof(MasterDbContextLocator))) throw new InvalidOperationException("Prevent duplicate registration of default DbContext");
 
             // 注册数据库上下文
-            return services.AddSqlServerPool<TDbContext, MasterDbContextLocator>(connectionString, poolSize, interceptors);
+            return services.AddSqlServerPool<TDbContext, MasterDbContextLocator>(connectionString, poolSize, dynamicDbContext, interceptors);
         }
 
         /// <summary>
@@ -52,9 +54,10 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="services">服务</param>
         /// <param name="connectionString">连接字符串</param>
         /// <param name="poolSize">池大小</param>
+        /// <param name="dynamicDbContext">动态数据库上下文，用于分表分库用</param>
         /// <param name="interceptors">拦截器</param>
         /// <returns>服务集合</returns>
-        public static IServiceCollection AddSqlServerPool<TDbContext, TDbContextLocator>(this IServiceCollection services, string connectionString = default, int poolSize = 100, params IInterceptor[] interceptors)
+        public static IServiceCollection AddSqlServerPool<TDbContext, TDbContextLocator>(this IServiceCollection services, string connectionString = default, int poolSize = 100, bool dynamicDbContext = false, params IInterceptor[] interceptors)
             where TDbContext : DbContext
             where TDbContextLocator : class, IDbContextLocator
         {
@@ -63,7 +66,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             // 配置数据库上下文
             var connStr = Penetrates.GetDbContextConnectionString<TDbContext>(connectionString);
-            services.AddDbContextPool<TDbContext>(Penetrates.ConfigureDbContext(connStr, options => ConfigureSqlServer(connStr, options), interceptors), poolSize: poolSize);
+            services.AddDbContextPool<TDbContext>(Penetrates.ConfigureDbContext(connStr, options => ConfigureSqlServer(connStr, options, dynamicDbContext), interceptors), poolSize: poolSize);
 
             return services;
         }
@@ -75,15 +78,16 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="services">服务</param>
         /// <param name="connectionString">连接字符串</param>
         /// <param name="interceptors">拦截器</param>
+        /// <param name="dynamicDbContext">动态数据库上下文，用于分表分库用</param>
         /// <returns>服务集合</returns>
-        public static IServiceCollection AddSqlServer<TDbContext>(this IServiceCollection services, string connectionString = default, params IInterceptor[] interceptors)
+        public static IServiceCollection AddSqlServer<TDbContext>(this IServiceCollection services, string connectionString = default, bool dynamicDbContext = false, params IInterceptor[] interceptors)
             where TDbContext : DbContext
         {
             // 避免重复注册默认数据库上下文
             if (Penetrates.DbContextWithLocatorCached.ContainsKey(typeof(MasterDbContextLocator))) throw new InvalidOperationException("Prevent duplicate registration of default DbContext");
 
             // 注册数据库上下文
-            return services.AddSqlServer<TDbContext, MasterDbContextLocator>(connectionString, interceptors);
+            return services.AddSqlServer<TDbContext, MasterDbContextLocator>(connectionString, dynamicDbContext, interceptors);
         }
 
         /// <summary>
@@ -93,9 +97,10 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <typeparam name="TDbContextLocator">数据库上下文定位器</typeparam>
         /// <param name="services">服务</param>
         /// <param name="connectionString">连接字符串</param>
+        /// <param name="dynamicDbContext">动态数据库上下文，用于分表分库用</param>
         /// <param name="interceptors">拦截器</param>
         /// <returns>服务集合</returns>
-        public static IServiceCollection AddSqlServer<TDbContext, TDbContextLocator>(this IServiceCollection services, string connectionString = default, params IInterceptor[] interceptors)
+        public static IServiceCollection AddSqlServer<TDbContext, TDbContextLocator>(this IServiceCollection services, string connectionString = default, bool dynamicDbContext = false, params IInterceptor[] interceptors)
             where TDbContext : DbContext
             where TDbContextLocator : class, IDbContextLocator
         {
@@ -104,19 +109,20 @@ namespace Microsoft.Extensions.DependencyInjection
 
             // 配置数据库上下文
             var connStr = Penetrates.GetDbContextConnectionString<TDbContext>(connectionString);
-            services.AddDbContext<TDbContext>(Penetrates.ConfigureDbContext(connStr, options => ConfigureSqlServer(connStr, options), interceptors));
+            services.AddDbContext<TDbContext>(Penetrates.ConfigureDbContext(connStr, options => ConfigureSqlServer(connStr, options, dynamicDbContext), interceptors));
 
             return services;
         }
 
         /// <summary>
-        /// 数据库 SqlServer
+        /// 配置 SqlServer
         /// </summary>
         /// <param name="connectionString">数据库连接字符串</param>
         /// <param name="options">数据库上下文选项构建器</param>
-        private static void ConfigureSqlServer(string connectionString, DbContextOptionsBuilder options)
+        /// <param name="dynamicDbContext">动态数据库上下文，用于分表分库用</param>
+        private static void ConfigureSqlServer(string connectionString, DbContextOptionsBuilder options, bool dynamicDbContext = false)
         {
-            options.UseSqlServer(connectionString, options =>
+            var dbContextOptionsBuilder = options.UseSqlServer(connectionString, options =>
             {
                 // 配置全局切割 Sql，而不是生成单个复杂 sql
                 //options.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
@@ -126,6 +132,10 @@ namespace Microsoft.Extensions.DependencyInjection
                 //options.EnableRetryOnFailure();
                 //options.MigrationsHistoryTable("__EFMigrationsHistory", "fur");
             });
+
+            // 解决分表分库
+            if (dynamicDbContext) dbContextOptionsBuilder
+                 .ReplaceService<IModelCacheKeyFactory, DynamicModelCacheKeyFactory>();
         }
     }
 }
