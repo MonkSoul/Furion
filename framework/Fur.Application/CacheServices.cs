049f83ed13b3ee6da0314f8be35d57aff6d6a82c
百小僧 <monksoul@outlook.com>
2020-10-18 18:51:09 +0800

    🤪 add cache documents.

---
+++ framework/Fur.Application/CacheServices.cs
@@ -0,0 +1,28 @@
+﻿using Fur.DynamicApiController;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace Fur.Application
+{
+    public class CacheServices : IDynamicApiController
+    {
+        private const string _timeCacheKey = "cache_time";
+
+        private readonly IMemoryCache _memoryCache;
+
+        public CacheServices(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [ApiDescriptionSettings(KeepName = true)]
+        public DateTimeOffset GetOrCreate()
+        {
+            return _memoryCache.GetOrCreate(_timeCacheKey, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(3);  // 滑动缓存时间
+                return DateTimeOffset.UtcNow;
+            });
+        }
+    }
+}
