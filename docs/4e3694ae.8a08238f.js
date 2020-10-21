e7b58b254f11f25d6a0659baad1f891092551ae5
Monk <monksoul@outlook.com>
2020-10-21 15:25:41 +0800

    🧤 update documents.

---
+++ docs/4e3694ae.8a08238f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{85:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"furos","name":"furos","count":1,"permalink":"/blog/tags/furos"}')}}]);
--- docs/4e3694ae.befe978c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{86:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"furos","name":"furos","count":1,"permalink":"/blog/tags/furos"}')}}]);
