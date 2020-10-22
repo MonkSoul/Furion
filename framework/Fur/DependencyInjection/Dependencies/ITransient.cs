9a05b9527a17240aab9f9a3ef86aed7138cfaa9e
Monk <monksoul@outlook.com>
2020-10-22 17:16:31 +0800

    🌧 remove file header comments.

--- framework/Fur/DependencyInjection/Dependencies/ITransient.cs
+++ framework/Fur/DependencyInjection/Dependencies/ITransient.cs
@@ -1,17 +1,4 @@
-﻿// -----------------------------------------------------------------------------
-// Fur 是 .NET 5 平台下企业应用开发最佳实践框架。
-// Copyright © 2020 Fur, Baiqian Co.,Ltd.
-//
-// 框架名称：Fur
-// 框架作者：百小僧
-// 框架版本：1.0.0-rc.final.20
-// 官方网站：https://chinadot.net
-// 源码地址：Gitee：https://gitee.com/monksoul/Fur
-// 				    Github：https://github.com/monksoul/Fur
-// 开源协议：Apache-2.0（http://www.apache.org/licenses/LICENSE-2.0）
-// -----------------------------------------------------------------------------
-
-namespace Fur.DependencyInjection
+﻿namespace Fur.DependencyInjection
 {
     /// <summary>
     /// 暂时服务注册依赖
