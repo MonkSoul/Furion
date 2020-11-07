18497872585396e7d5cabd427c1f9178dbeb31d9
百小僧 <monksoul@outlook.com>
2020-11-07 15:02:31 +0800

    🙂 remove unused code.

--- framework/Fur.Application/Persons/PersonService.cs
+++ framework/Fur.Application/Persons/PersonService.cs
@@ -87,7 +87,6 @@ namespace Fur.Application
         /// 查询所有
         /// </summary>
         /// <returns></returns>
-        //[NonUnify]
         public async Task<List<PersonDto>> GetAll()
         {
             var persons = _personRepository.AsQueryable(false)
