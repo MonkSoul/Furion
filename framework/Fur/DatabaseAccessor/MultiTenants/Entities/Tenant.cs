0a3a05fdaf724b698b4d026ba56ee57d6cf0ad67
Monk <monksoul@outlook.com>
2020-11-10 19:05:39 +0800

    😒 fixed Tenant database size.

--- framework/Fur/DatabaseAccessor/MultiTenants/Entities/Tenant.cs
+++ framework/Fur/DatabaseAccessor/MultiTenants/Entities/Tenant.cs
@@ -28,34 +28,37 @@ namespace Fur.DatabaseAccessor
         /// <summary>
         /// 租户名
         /// </summary>
-        [Required]
+        [Required, MaxLength(128)]
         public string Name { get; set; }
 
         /// <summary>
         /// 来源主机地址
         /// </summary>
+        [MaxLength(256)]
         public string Host { get; set; }
 
         /// <summary>
         /// 电子邮箱
         /// </summary>
-        [EmailAddress]
+        [EmailAddress, MaxLength(256)]
         public string EmailAddress { get; set; }
 
         /// <summary>
         /// 手机号码
         /// </summary>
-        [Phone]
+        [Phone, MaxLength(32)]
         public string PhoneNumber { get; set; }
 
         /// <summary>
         /// 架构名
         /// </summary>
+        [Phone, MaxLength(32)]
         public string Schema { get; set; }
 
         /// <summary>
         /// 数据库链接字符串
         /// </summary>
+        [MaxLength(256)]
         public string ConnectionString { get; set; }
 
         /// <summary>
