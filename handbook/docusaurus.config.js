2a50eff0e36f382183f1a94b42be76a3de3a4d10
Monk <monksoul@outlook.com>
2020-11-11 14:03:51 +0800

    🍸 update documents.

--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -30,8 +30,12 @@ module.exports = {
           position: "left",
         },
         { to: "blog", label: "博客", position: "left" },
-        { label: "配置", position: "left" },
-        { label: "视频", position: "right" },
+        { label: "社区", position: "left", href: "https://chinadot.net" },
+        {
+          label: "视频",
+          position: "right",
+          href: "https://space.bilibili.com/695987967",
+        },
         {
           label: "工具",
           position: "right",
@@ -70,7 +74,7 @@ module.exports = {
           items: [
             {
               label: "入门",
-              to: "docs",
+              to: "docs/get-start",
             },
             {
               label: "指南",
