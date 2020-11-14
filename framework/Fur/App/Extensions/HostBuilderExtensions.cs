11fb45858e188851bb0dcbd7db6079d4f3b56dee
百小僧 <monksoul@outlook.com>
2020-11-14 22:48:19 +0800

    😲 remove unused code.

--- framework/Fur/App/Extensions/HostBuilderExtensions.cs
+++ framework/Fur/App/Extensions/HostBuilderExtensions.cs
@@ -1,7 +1,5 @@
-﻿using Fur;
-using Fur.DependencyInjection;
+﻿using Fur.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Extensions.Hosting
 {
@@ -20,31 +18,6 @@ namespace Microsoft.Extensions.Hosting
         public static IWebHostBuilder Inject(this IWebHostBuilder hostBuilder, string assemblyName = nameof(Fur))
         {
             hostBuilder.UseSetting(WebHostDefaults.HostingStartupAssembliesKey, assemblyName);
-            return hostBuilder;
-        }
-
-        /// <summary>
-        /// 非 Web 主机注入
-        /// </summary>
-        /// <param name="hostBuilder"></param>
-        /// <returns></returns>
-        public static IHostBuilder Inject(this IHostBuilder hostBuilder)
-        {
-            hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                InternalApp.AddConfigureFiles(config, hostingContext.HostingEnvironment);
-            });
-
-            // 自动注入 AddApp() 服务
-            hostBuilder.ConfigureServices(services =>
-            {
-                // 添加全局配置和存储服务提供器
-                InternalApp.InternalServices = services;
-
-                // 初始化应用服务
-                services.AddApp();
-            });
-
             return hostBuilder;
         }
     }
