93eb3f84185558751484875472bca070384706ab
Monk <monksoul@outlook.com>
2020-12-08 15:08:25 +0800

    🍝 close #I28KRP

--- framework/Furion/App/Models/InternalApp.cs
+++ framework/Furion/App/Models/InternalApp.cs
@@ -128,10 +128,11 @@ namespace Furion
             "appsettings.json",
             "appsettings.Development.json",
             "appsettings.Production.json",
+            "bundleconfig.json"
         };
 
         /// <summary>
-        /// 运行时 Json 后缀
+        /// 排除运行时 Json 后缀
         /// </summary>
         private static readonly string[] runtimeJsonSuffixs = new[]
         {
