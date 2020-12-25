513e1f86ca52e23d3873680937f28090aa97155a
Argo-Cloud <argo@163.com>
2020-12-25 16:08:40 +0800

    feat: add PetaPoco orm extension lib

---
+++ framework/Furion.Extras.DatabaseAccessor.PetaPoco/Extensions/PetaPocoServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using PetaPoco;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Dapper 拓展类
+    /// </summary>
+    public static class PetaPocoServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 添加 Dapper 拓展
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="optionsAction"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddPetaPoco(this IServiceCollection services, Action<IDatabaseBuildConfiguration> optionsAction)
+        {
+            services.AddTransient<IDatabase>(sp =>
+            {
+                var builder = DatabaseConfiguration.Build();
+                optionsAction(builder);
+                return new Database(builder);
+            });
+            return services;
+        }
+    }
+}
