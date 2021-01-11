88db212b33c41b35ab2b22484262ca3c2c8b0393
YaChengMu <1140049363@qq.com>
2021-01-11 21:43:42 +0800

    add framework/Furion.Extras.DatabaseAccessor.SqlSugar/Extensions/PagedQueryableExtensions.cs. 增加ISugarQueryable<TEntity>分页拓展方法

---
+++ framework/Furion.Extras.DatabaseAccessor.SqlSugar/Extensions/PagedQueryableExtensions.cs
@@ -0,0 +1,62 @@
+using SqlSugar;
+using System;
+using System.Threading.Tasks;
+
+namespace Furion.DatabaseAccessor
+{
+    /// <summary>
+    /// 分页拓展类
+    /// </summary>
+    public static class PagedQueryableExtensions
+    {
+        /// <summary>
+        /// 分页拓展
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static FurionPagedModel<TEntity> ToPagedList<TEntity>(this ISugarQueryable<TEntity> entity, int pageIndex, int pageSize)
+                      where TEntity : class, new()
+        {
+            int totalCount = 0;
+            var items = entity.ToPageList(pageIndex, pageSize, ref totalCount);
+            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            return new FurionPagedModel<TEntity>
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                Items = items,
+                TotalCount = (int)totalCount,
+                TotalPages = totalPages,
+                HasNextPages = pageIndex < totalPages,
+                HasPrevPages = pageIndex - 1 > 0
+            };
+        }
+
+        /// <summary>
+        /// 分页拓展
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<FurionPagedModel<TEntity>> ToPagedListAsync<TEntity>(this ISugarQueryable<TEntity> entity, int pageIndex, int pageSize)
+                      where TEntity : class, new()
+        {
+            RefAsync<int> totalCount = 0;
+            var items = await entity.ToPageListAsync(pageIndex, pageSize, totalCount);
+            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            return new FurionPagedModel<TEntity>
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                Items = items,
+                TotalCount = (int)totalCount,
+                TotalPages = totalPages,
+                HasNextPages = pageIndex < totalPages,
+                HasPrevPages = pageIndex - 1 > 0
+            };
+        }
+    }
+}
