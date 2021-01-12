348287093519039b1be4c6e93c02471521cdfae8
百小僧 <monksoul@outlook.com>
2021-01-12 21:32:56 +0800

    🍝 update object mapper documents.

---
+++ docs/1f1c7772.13f7e770.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{83:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Furion \u4ecb\u7ecd","permalink":"/blog/welcome"}]}')}}]);
