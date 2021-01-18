99931af78bb523ca486746a5e95703aa87e6b8b6
Monk <monksoul@outlook.com>
2021-01-18 10:49:43 +0800

    👳‍♀️ update documents.

--- docs/1f1c7772.180dd4fe.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{85:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Furion \u4ecb\u7ecd","permalink":"/blog/welcome"}]}')}}]);
