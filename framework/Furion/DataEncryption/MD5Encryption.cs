3201cae53776a6327807ca572170db4fee2b9c1d
Executor-Cheng <exprotected@outlook.com>
2021-01-23 18:17:27 +0800

    Changed back to unsafe methods, since they had better performance. See more information in pr.

--- framework/Furion/DataEncryption/MD5Encryption.cs
+++ framework/Furion/DataEncryption/MD5Encryption.cs
@@ -1,6 +1,6 @@
 ﻿using Furion.DependencyInjection;
 using System;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -10,9 +10,24 @@ namespace Furion.DataEncryption
     /// MD5 加密
     /// </summary>
     [SkipScan]
-    public class MD5Encryption
+    public static unsafe class MD5Encryption
     {
-        private static readonly char[] Digitals = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        private const uint LOWERCASING = 0x2020U;
+
+        private static readonly delegate* managed<ReadOnlySpan<byte>, Span<char>, uint, void> _EncodeToUtf16Ptr;
+
+        [ThreadStatic]
+        private static MD5 instance;
+
+        /// <summary>
+        /// MD5实例
+        /// </summary>
+        public static MD5 Instance => instance ??= MD5.Create();
+
+        static MD5Encryption()
+        {
+            _EncodeToUtf16Ptr = (delegate* managed<ReadOnlySpan<byte>, Span<char>, uint, void>)typeof(uint).Assembly.GetType("System.HexConverter").GetMethod("EncodeToUtf16", BindingFlags.Static | BindingFlags.Public).MethodHandle.GetFunctionPointer();
+        }
 
         /// <summary>
         /// 字符串 MD5 比较
@@ -33,51 +48,13 @@ namespace Furion.DataEncryption
         /// <returns></returns>
         public static string Encrypt(string text)
         {
-            var bytes = Encoding.UTF8.GetBytes(text);
-
-            return ByteToString(MD5Instances.Instance.ComputeHash(bytes));
-        }
-
-        /// <summary>
-        /// 创建MD5实例
-        /// </summary>
-        private static class MD5Instances
-        {
-            /// <summary>
-            /// 线程静态变量
-            /// </summary>
-            [ThreadStatic]
-            private static MD5 instance;
-
-            /// <summary>
-            /// MD5实例
-            /// </summary>
-            public static MD5 Instance => instance ?? Create();
-
-            [MethodImpl(MethodImplOptions.NoInlining)]
-            private static MD5 Create()
+            return string.Create(32, text, static (p, q) =>
             {
-                instance = MD5.Create() ?? Activator.CreateInstance<MD5>();
-
-                return instance;
-            }
-        }
-
-        /// <summary>
-        /// 重写ToString方法
-        /// </summary>
-        /// <param name="bytes"></param>
-        /// <returns></returns>
-        private static string ByteToString(byte[] bytes)
-        {
-            var chars = new char[bytes.Length * 2];
-            var index = 0;
-            foreach (var item in bytes)
-            {
-                chars[index] = Digitals[item >> 4]; ++index;
-                chars[index] = Digitals[item & 15]; ++index;
-            }
-            return new string(chars, 0, chars.Length);
+                byte[] buffer = Encoding.UTF8.GetBytes(q);
+                Span<byte> signed = stackalloc byte[16];
+                Instance.TryComputeHash(buffer, signed, out _);
+                _EncodeToUtf16Ptr(signed, p, LOWERCASING);
+            });
         }
     }
 }
