87c4421ca49f08e1e71274245027eeaef5563ca0
Monk <monksoul@outlook.com>
2021-01-27 17:57:40 +0800

    🎞 update documents.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -129,7 +129,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#723cff" }}>2340</div>
+            <div style={{ color: "#723cff" }}>2350</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
