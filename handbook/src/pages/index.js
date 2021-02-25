1269d0969138d53a89f358f660a56b09f15b6c69
Monk <monksoul@outlook.com>
2021-02-25 17:12:29 +0800

    🌯 update documents.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -131,7 +131,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#723cff" }}>2700</div>
+            <div style={{ color: "#723cff" }}>2720</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
@@ -155,7 +155,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>793,086</div>
+            <div style={{ color: "#1fd898" }}>801,417</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
