3dcd6878c3ca5001b49b252d54f6496cb765efce
Monk <monksoul@outlook.com>
2021-03-04 16:30:15 +0800

    🤐 update UnitTests Project.

---
+++ unittest/Furion.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Furion.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
