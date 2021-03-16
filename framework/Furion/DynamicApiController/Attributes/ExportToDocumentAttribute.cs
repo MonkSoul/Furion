f0d43dabce2b82222b7179328cca86b778bb17ab
Monk <monksoul@outlook.com>
2021-03-16 19:25:32 +0800

    😎 close #I3BJI9.

---
+++ framework/Furion/DynamicApiController/Attributes/ExportToDocumentAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Furion.DependencyInjection;
+using System;
+
+namespace Furion.DynamicApiController
+{
+    /// <summary>
+    /// 配置 Mvc 控制器也能导出到 Swagger 中
+    /// </summary>
+    [SkipScan, AttributeUsage(AttributeTargets.Class)]
+    public sealed class ExportToDocumentAttribute : Attribute
+    {
+    }
+}
