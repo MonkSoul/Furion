e9abd7d955f53da70ba016b028270f5e4b7750c3
Monk <monksoul@outlook.com>
2021-03-22 12:36:33 +0800

    🎅 update documents.

--- handbook/src/components/StarProject.js
+++ handbook/src/components/StarProject.js
@@ -3,17 +3,24 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import "./StarProject.css";
 
 export default function StarProject() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
 
   return (
     <div className="furion-star-project">
-      <a
-        href="https://gitee.com/zuohuaijun/Admin.NET"
-        target="_blank"
-        title="dotNET China 精选项目第 01 届"
-      >
-        <img src={useBaseUrl("img/Admin.NET.png")} />
-      </a>
+      <button className="furion-star-close" onClick={() => setShow(false)}>
+        关闭
+      </button>
+      {show ? (
+        <a
+          href="https://gitee.com/zuohuaijun/Admin.NET"
+          target="_blank"
+          title="dotNET China 精选项目第 01 届"
+        >
+          <img src={useBaseUrl("img/Admin.NET.png")} />
+        </a>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
