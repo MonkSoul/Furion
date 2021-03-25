e4f5d28c659da7840ac79c200d492e4f9139ef34
Monk <monksoul@outlook.com>
2021-03-25 11:12:15 +0800

    ✨ update documents.

--- docs/assets/js/6820b113.b5a0db45.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{125:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"download","name":"download","count":1,"permalink":"/blog/tags/download"}')}}]);
