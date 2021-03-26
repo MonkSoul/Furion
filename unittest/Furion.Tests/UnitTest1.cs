8e668354a68a0be43cf8cda64e335075e38bb0da
Monk <monksoul@outlook.com>
2021-03-26 14:05:04 +0800

    🍟 更新单元测试代码

--- unittest/Furion.Tests/UnitTest1.cs
+++ unittest/Furion.Tests/UnitTest1.cs
@@ -1,7 +1,4 @@
-using Furion.DataEncryption;
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using MSTest.Extensions.Contracts;
 
 namespace Furion.Tests
@@ -17,29 +14,5 @@ namespace Furion.Tests
                 Assert.AreEqual("Furion", "Furion");
             });
         }
-
-
-        /// <summary>
-        /// ≤‚ ‘º”√‹
-        /// </summary>
-        [TestMethod]
-        public void TestDataEncryption()
-        {
-            var a = "123465".ToMd5();
-
-            var b1 = "123465".ToAesEncry();
-            var b2 = b1.ToAesDecry();
-
-            var b3 = "123465".ToAesEncry("abc123");
-            var b4 = b3.ToAesDecry("abc123");
-
-            var c1 = "123123".ToDescEncry();
-            var c2 = c1.ToDescDecry();
-
-            var c3 = "123123".ToDescEncry("abc123");
-            var c4 = c3.ToDescDecry("abc123");
-
-        }
-
     }
 }
