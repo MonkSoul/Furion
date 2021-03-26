bec2fa61de484b744c461c121191fdd397e08a0d
百小僧 <monksoul@outlook.com>
2021-03-26 22:53:48 +0800

    🥱 update samples.

--- samples/Furion.Application/RBAC/Dtos/LoginInput.cs
+++ samples/Furion.Application/RBAC/Dtos/LoginInput.cs
@@ -10,12 +10,14 @@ namespace Furion.Application
         /// <summary>
         /// 用户名
         /// </summary>
+        /// <example>admin</example>
         [Required, MinLength(3)]
         public string Account { get; set; }
 
         /// <summary>
         /// 密码
         /// </summary>
+        /// <example>admin</example>
         [Required, MinLength(5)]
         public string Password { get; set; }
     }
