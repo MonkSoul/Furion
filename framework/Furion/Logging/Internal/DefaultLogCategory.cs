e7ded3ccc632d20ceb436323bc99439d811fb2c6
百小僧 <monksoul@outlook.com>
2021-04-06 00:05:56 +0800

    🧑 adjust framework code.

---
+++ framework/Furion/Logging/Internal/DefaultLogCategory.cs
@@ -0,0 +1,12 @@
+﻿using Furion.DependencyInjection;
+
+namespace Furion.Logging
+{
+    /// <summary>
+    /// 默认日志分类
+    /// </summary>
+    [SkipScan]
+    public sealed class DefaultLogCategory
+    {
+    }
+}
