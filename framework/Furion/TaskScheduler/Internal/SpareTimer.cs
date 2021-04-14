87052bb9b82efc573351e9e6ee004a154404a3f7
Monk <monksoul@outlook.com>
2021-04-14 15:28:34 +0800

    😃 link #I3IRUX.

---
+++ framework/Furion/TaskScheduler/Internal/SpareTimer.cs
@@ -0,0 +1,43 @@
+﻿using Furion.DependencyInjection;
+using System;
+using System.Timers;
+
+namespace Furion.TaskScheduler
+{
+    /// <summary>
+    /// 内置时间调度器
+    /// </summary>
+    [SkipScan]
+    public sealed class SpareTimer : Timer
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="workerName"></param>
+        internal SpareTimer(string workerName = default) : base()
+        {
+            WorkerName = workerName ?? Guid.NewGuid().ToString("N");
+
+            // 存储当前 Worker
+            SpareTime.Workers.TryAdd(WorkerName, this);
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="interval"></param>
+        /// <param name="workerName"></param>
+        internal SpareTimer(double interval, string workerName = default) : base(interval)
+        {
+            WorkerName = workerName ?? Guid.NewGuid().ToString("N");
+
+            // 存储当前 Worker
+            SpareTime.Workers.TryAdd(WorkerName, this);
+        }
+
+        /// <summary>
+        /// 当前任务名
+        /// </summary>
+        public string WorkerName { get; private set; }
+    }
+}
--- framework/Furion/TaskScheduler/SpareTime.cs
+++ framework/Furion/TaskScheduler/SpareTime.cs
@@ -1,6 +1,6 @@
 ﻿using Furion.DependencyInjection;
 using System;
-using System.Timers;
+using System.Collections.Concurrent;
 
 namespace Furion.TaskScheduler
 {
@@ -11,24 +11,34 @@ namespace Furion.TaskScheduler
     public static class SpareTime
     {
         /// <summary>
-        /// 执行简单任务
+        /// 记录所有任务
         /// </summary>
-        /// <param name="interval"></param>
-        /// <param name="continued"></param>
+        internal static ConcurrentDictionary<string, SpareTimer> Workers;
+
+        /// <summary>
+        /// 静态构造函数
+        /// </summary>
+        static SpareTime()
+        {
+            Workers = new ConcurrentDictionary<string, SpareTimer>();
+        }
+
+        /// <summary>
+        /// 开始执行简单任务
+        /// </summary>
+        /// <param name="interval">时间间隔（毫秒）</param>
+        /// <param name="continued">是否持续执行</param>
         /// <param name="doWhat"></param>
-        /// <param name="timer"></param>
-        public static void Do(double interval, bool continued = true, Action<Timer> doWhat = default, Timer timer = default)
+        /// <param name="workerName"></param>
+        public static void Do(double interval, bool continued = true, Action<SpareTimer> doWhat = default, string workerName = default)
         {
             if (doWhat == null) return;
 
-            timer ??= new Timer(interval);
+            var timer = new SpareTimer(interval, workerName);
             timer.Elapsed += (sender, e) =>
             {
-                if (!continued)
-                {
-                    timer.Stop();
-                    timer.Dispose();
-                }
+                // 停止任务
+                if (!continued) Cancel(timer.WorkerName);
 
                 doWhat(timer);
             };
@@ -37,38 +47,40 @@ namespace Furion.TaskScheduler
         }
 
         /// <summary>
-        /// 执行简单任务（持续的）
+        /// 开始执行简单任务（持续的）
         /// </summary>
-        /// <param name="interval"></param>
+        /// <param name="interval">时间间隔（毫秒）</param>
         /// <param name="doWhat"></param>
-        /// <param name="timer"></param>
-        public static void Do(double interval, Action<Timer> doWhat = default, Timer timer = default)
+        /// <param name="workerName"></param>
+        public static void Do(double interval, Action<SpareTimer> doWhat = default, string workerName = default)
         {
-            Do(interval, true, doWhat, timer);
+            Do(interval, true, doWhat, workerName);
         }
 
         /// <summary>
-        /// 执行简单任务（只执行一次）
+        /// 开始执行简单任务（只执行一次）
         /// </summary>
-        /// <param name="interval"></param>
+        /// <param name="interval">时间间隔（毫秒）</param>
         /// <param name="doWhat"></param>
-        /// <param name="timer"></param>
-        public static void DoOnce(double interval, Action<Timer> doWhat = default, Timer timer = default)
+        /// <param name="workerName"></param>
+        public static void DoOnce(double interval, Action<SpareTimer> doWhat = default, string workerName = default)
         {
-            Do(interval, false, doWhat, timer);
+            Do(interval, false, doWhat, workerName);
         }
 
         /// <summary>
-        /// 执行 Cron 表达式任务
+        /// 开始执行 Cron 表达式任务
         /// </summary>
-        /// <param name="expression"></param>
+        /// <param name="expression">Cron 表达式</param>
         /// <param name="doWhat"></param>
-        /// <param name="timer"></param>
-        public static void Do(string expression, Action<Timer> doWhat = default, Timer timer = default)
+        /// <param name="workerName"></param>
+        public static void Do(string expression, Action<SpareTimer> doWhat = default, string workerName = default)
         {
             if (doWhat == null) return;
 
-            Do(1000, e =>
+            workerName ??= Guid.NewGuid().ToString("N");
+
+            Do(1000, timer =>
             {
                 // 解析 Cron 表达式
                 var cronExpression = CronExpression.Parse(expression, CronFormat.IncludeSeconds);
@@ -76,10 +88,64 @@ namespace Furion.TaskScheduler
                 // 获取下一个执行时间
                 var nextTime = cronExpression.GetNextOccurrence(DateTimeOffset.Now, TimeZoneInfo.Local);
                 var nextLocalTime = nextTime?.DateTime;
-                if (nextLocalTime == null) e.Stop();
 
-                DoOnce((nextLocalTime.Value - DateTime.Now).TotalSeconds, se => doWhat(se));
-            }, timer);
+                // 停止任务
+                if (nextLocalTime == null) Cancel(timer.WorkerName);
+
+                // 执行任务
+                DoOnce((nextLocalTime.Value - DateTime.Now).TotalSeconds, subTimer => doWhat(subTimer), $"{workerName}[{expression}]");
+            }, workerName);
+        }
+
+        /// <summary>
+        /// 开始某个任务
+        /// </summary>
+        /// <param name="workerName"></param>
+        public static void Start(string workerName)
+        {
+            if (string.IsNullOrWhiteSpace(workerName)) throw new ArgumentNullException(workerName);
+
+            // 判断任务是否存在
+            if (!Workers.TryGetValue(workerName, out var timer)) return;
+
+            // 启动任务
+            if (!timer.Enabled) timer.Start();
+        }
+
+        /// <summary>
+        /// 开始某个任务
+        /// </summary>
+        /// <param name="workerName"></param>
+        public static void Stop(string workerName)
+        {
+            if (string.IsNullOrWhiteSpace(workerName)) throw new ArgumentNullException(workerName);
+
+            // 判断任务是否存在
+            if (!Workers.TryGetValue(workerName, out var timer)) return;
+
+            // 停止任务
+            if (timer.Enabled) timer.Stop();
+        }
+
+        /// <summary>
+        /// 取消某个任务
+        /// </summary>
+        /// <param name="workerName"></param>
+        public static void Cancel(string workerName)
+        {
+            if (string.IsNullOrWhiteSpace(workerName)) throw new ArgumentNullException(workerName);
+
+            // 判断任务是否存在
+            if (!Workers.TryGetValue(workerName, out var timer)) return;
+
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                timer.Dispose();
+            }
+
+            // 移除
+            _ = Workers.TryRemove(workerName, out _);
         }
     }
 }
