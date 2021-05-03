6d25974098e65ae8259e8fc26bd55b34cc3599c1
百小僧 <monksoul@outlook.com>
2021-05-03 16:48:34 +0800

    👶 publish v2.2.4 version.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -163,7 +163,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>221,971</div>
+            <div style={{ color: "#1fd898" }}>222,489</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
