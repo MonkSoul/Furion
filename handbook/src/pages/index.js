495c8e9dec7f7a108af67bb9482ff6733bdfd7b0
Monk <monksoul@outlook.com>
2021-05-13 11:41:04 +0800

    🎨 publish v2.4.1 version.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -81,7 +81,7 @@ function Banner() {
             >
               一分钟上手
             </Link>
-            <div className="furion-version">v2.4.0</div>
+            <div className="furion-version">v2.4.1</div>
           </div>
         </div>
         <div className="furion-banner-item">
