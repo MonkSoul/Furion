a534c3d354201b5ea6e6efe3f10e497b21a986a2
Monk <monksoul@outlook.com>
2021-05-13 15:38:47 +0800

    🎭 remove unused code.

--- samples/Furion.Application/Persons/PersonService.cs
+++ samples/Furion.Application/Persons/PersonService.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -119,10 +118,6 @@ namespace Furion.Application
                                            .ProjectToType<PersonDto>();
 
             return await persons.ToListAsync();
-        }
-
-        public void 测试脱敏词([SensitiveDetection] string txt)
-        {
         }
     }
 }
