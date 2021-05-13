08403cf1c6d598ad94d76303ca225589f5fad0c7
百小僧 <monksoul@outlook.com>
2021-05-13 23:08:02 +0800

    👕 update documents.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>269,574</div>
+            <div style={{ color: "#1fd898" }}>271,977</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
