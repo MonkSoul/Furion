571fbf3cec3b8d56ba40916cf7b7fa61a6758400
百小僧 <monksoul@outlook.com>
2021-05-16 22:33:56 +0800

    🎭 publish v2.5.1 version.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -81,7 +81,7 @@ function Banner() {
             >
               一分钟上手
             </Link>
-            <div className="furion-version">v2.5.0</div>
+            <div className="furion-version">v2.5.1</div>
           </div>
         </div>
         <div className="furion-banner-item">
