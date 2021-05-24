9f9e022ead4ac210f712285945a801c7817023eb
Monk <monksoul@outlook.com>
2021-05-24 09:41:35 +0800

    🤗 clean up framework code.

--- framework/Furion/TaskScheduler/Attributes/SpareTimeAttribute.cs
+++ framework/Furion/TaskScheduler/Attributes/SpareTimeAttribute.cs
@@ -5,8 +5,8 @@
 // 框架名称：Furion
 // 框架作者：百小僧
 // 框架版本：2.6.6
-// 源码地址：Gitee： https://gitee.com/dotnetchina/Furion 
-//          Github：https://github.com/monksoul/Furion 
+// 源码地址：Gitee： https://gitee.com/dotnetchina/Furion
+//          Github：https://github.com/monksoul/Furion
 // 开源协议：Apache-2.0（https://gitee.com/dotnetchina/Furion/blob/master/LICENSE）
 // -----------------------------------------------------------------------------
 
