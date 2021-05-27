21b53b4fd74ceb85f5afe456fa3be5166454376f
Monk <monksoul@outlook.com>
2021-05-27 16:18:06 +0800

    🦺 update documents.

---
+++ framework/Furion/DistributedIDGenerator/ShortID/Internal/RandomHelpers.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------------
+// 让 .NET 开发更简单，更通用，更流行。
+// Copyright © 2020-2021 Furion, 百小僧, Baiqian Co.,Ltd.
+//
+// 框架名称：Furion
+// 框架作者：百小僧
+// 框架版本：2.7.3
+// 源码地址：Gitee： https://gitee.com/dotnetchina/Furion
+//          Github：https://github.com/monksoul/Furion
+// 开源协议：Apache-2.0（https://gitee.com/dotnetchina/Furion/blob/master/LICENSE）
+// -----------------------------------------------------------------------------
+
+using System;
+
+namespace Furion.DistributedIDGenerator
+{
+    /// <summary>
+    /// 随机数帮助类
+    /// </summary>
+    internal static class RandomHelpers
+    {
+        /// <summary>
+        /// 随机数对象
+        /// </summary>
+        private static readonly Random Random = new();
+
+        /// <summary>
+        /// 线程锁
+        /// </summary>
+        private static readonly object ThreadLock = new();
+
+        /// <summary>
+        /// 生成线程安全的范围内随机数
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int GenerateNumberInRange(int min, int max)
+        {
+            lock (ThreadLock)
+            {
+                return Random.Next(min, max);
+            }
+        }
+    }
+}
---
+++ framework/Furion/DistributedIDGenerator/ShortID/Options/GenerationOptions.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------------
+// 让 .NET 开发更简单，更通用，更流行。
+// Copyright © 2020-2021 Furion, 百小僧, Baiqian Co.,Ltd.
+//
+// 框架名称：Furion
+// 框架作者：百小僧
+// 框架版本：2.7.3
+// 源码地址：Gitee： https://gitee.com/dotnetchina/Furion
+//          Github：https://github.com/monksoul/Furion
+// 开源协议：Apache-2.0（https://gitee.com/dotnetchina/Furion/blob/master/LICENSE）
+// -----------------------------------------------------------------------------
+
+using Furion.DependencyInjection;
+
+namespace Furion.DistributedIDGenerator
+{
+    /// <summary>
+    /// 短 ID 生成配置选项
+    /// </summary>
+    [SkipScan]
+    public class GenerationOptions
+    {
+        /// <summary>
+        /// 是否使用数字
+        /// <para>默认 false</para>
+        /// </summary>
+        public bool UseNumbers { get; set; }
+
+        /// <summary>
+        /// 是否使用特殊字符
+        /// <para>默认 true</para>
+        /// </summary>
+        public bool UseSpecialCharacters { get; set; } = true;
+
+        /// <summary>
+        /// 设置短 ID 长度
+        /// </summary>
+        public int Length { get; set; } = RandomHelpers.GenerateNumberInRange(Constants.MinimumAutoLength, Constants.MaximumAutoLength);
+    }
+}
