47896123dcb11fb9e0fd29e50f6a36aaa16c0244
付金龙 <fujinlong@bjknrt.com>
2021-05-28 18:30:57 +0800

    配置项继承IConfigurableOptions

--- framework/Furion/UrlRewriter/Options/UrlRewriteSettingsOptions.cs
+++ framework/Furion/UrlRewriter/Options/UrlRewriteSettingsOptions.cs
@@ -1,4 +1,7 @@
-﻿using Furion.DependencyInjection;
+﻿using Furion.ConfigurableOptions;
+using Furion.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace Furion.UrlRewriter
 {
@@ -6,7 +9,7 @@ namespace Furion.UrlRewriter
     /// URL转发选项
     /// </summary>
     [SkipScan]
-    public class UrlRewriteSettingsOptions
+    public class UrlRewriteSettingsOptions : IConfigurableOptions<UrlRewriteSettingsOptions>
     {
         /// <summary>
         /// 是否启用URL转发规则
@@ -17,5 +20,15 @@ namespace Furion.UrlRewriter
         /// URL转发规则列表
         /// </summary>
         public string[][] UrlRewriteRules { get; set; }
+
+        /// <summary>
+        /// 配置后期处理
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="configuration"></param>
+        public void PostConfigure(UrlRewriteSettingsOptions options, IConfiguration configuration)
+        {
+            options.UrlRewriteRules ??= Array.Empty<string[]>();
+        }
     }
 }
