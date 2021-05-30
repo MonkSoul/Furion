950a68736c35d2065b14629dd0dfac0e0339e2ff
百小僧 <monksoul@outlook.com>
2021-05-30 21:05:24 +0800

    🚈 update documents.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -140,7 +140,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#723cff" }}>5400 +</div>
+            <div style={{ color: "#723cff" }}>5410 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>363,929</div>
+            <div style={{ color: "#1fd898" }}>365,749</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
