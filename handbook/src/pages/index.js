c39c2692c6e8c10a92cf8aef666c3c4f3a138b9d
百小僧 <monksoul@outlook.com>
2021-06-03 22:28:01 +0800

    🎅 update documents

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -140,7 +140,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#723cff" }}>5500 +</div>
+            <div style={{ color: "#723cff" }}>5510 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
@@ -152,7 +152,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#3fbbfe" }}>2100 +</div>
+            <div style={{ color: "#3fbbfe" }}>2140 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Forks</span>
           </div>
         </div>
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>373,748</div>
+            <div style={{ color: "#1fd898" }}>377,945</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
