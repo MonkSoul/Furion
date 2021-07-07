76dcfcbd9f7cb69739e995cd62dd25a152e1a1d4
百小僧 <monksoul@outlook.com>
2021-07-08 00:40:47 +0800

    🤣 publish v2.12.1 version to mainly optimize multithreading performance.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -81,7 +81,7 @@ function Banner() {
             >
               一分钟上手
             </Link>
-            <div className="furion-version">v2.12.0</div>
+            <div className="furion-version">v2.12.1</div>
           </div>
         </div>
         <div className="furion-banner-item">
