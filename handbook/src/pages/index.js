86b09da0ef1d4bb2249f05120bc6c0e1f162e504
Monk <monksoul@outlook.com>
2021-07-16 09:25:59 +0800

    😋 update CHANGELOG.md and Nuget download times.

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -140,7 +140,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#723cff" }}>6650 +</div>
+            <div style={{ color: "#723cff" }}>6700 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>708,629</div>
+            <div style={{ color: "#1fd898" }}>737,176</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
