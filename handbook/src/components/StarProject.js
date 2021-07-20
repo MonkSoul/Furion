3b9859e138646f08023c22a2313ba513e8ef2d1c
Monk <monksoul@outlook.com>
2021-07-20 12:20:46 +0800

    📁 通过 Jenkins 自动化发布文档

--- handbook/src/components/StarProject.js
+++ handbook/src/components/StarProject.js
@@ -6,23 +6,10 @@ export default function StarProject() {
   const [show, setShow] = useState(true);
 
   return (
-    <div className="furion-star-project" onClick={() => setShow(!show)}>
-      {show && (
-        <div className="furion-star-title" style={{ color: "white" }}>
-          微信公众号
-        </div>
-      )}
-      <button className="furion-star-close">{show ? "收起" : "展开"}</button>
-      <img
-        src={useBaseUrl("img/monksoul.jpg")}
-        style={{ height: show ? null : 100 }}
-        title="打开微信扫一扫关注百小僧公众号"
-      />
-      {show && (
-        <div className="furion-star-title" style={{ color: "yellow" }}>
-          百小僧别吹牛
-        </div>
-      )}
+    <div className="furion-star-project">
+      <a href="https://gitee.com/monksoul" target="_blank">
+        <img src={useBaseUrl("img/icon.png")} title="点击了解百小僧" />
+      </a>
     </div>
   );
 }
