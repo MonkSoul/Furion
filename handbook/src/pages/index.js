f921ebc4fc3261c4e9cbaeadf1f0ea98c8dfb8d7
百小僧 <monksoul@outlook.com>
2021-07-25 21:46:33 +0800

    📁 通过 Jenkins 自动化发布 v2.15.5 文档

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -81,7 +81,7 @@ function Banner() {
             >
               一分钟上手
             </Link>
-            <div className="furion-version">v2.15.4</div>
+            <div className="furion-version">v2.15.5</div>
           </div>
         </div>
         <div className="furion-banner-item">
