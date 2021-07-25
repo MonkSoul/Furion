7efe51634c2820e7138df6f684e8fcb5120ef9f4
百小僧 <monksoul@outlook.com>
2021-07-25 21:48:15 +0800

    🤣 添加单元测试初始化类注释

--- unittest/Furion.UnitTests/Initialize.cs
+++ unittest/Furion.UnitTests/Initialize.cs
@@ -3,9 +3,13 @@ using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
+// 配置启动类类型
 [assembly: TestFramework("Furion.UnitTests.Initialize", "Furion.UnitTests")]
 namespace Furion.UnitTests
 {
+    /// <summary>
+    /// 单元测试初始化类
+    /// </summary>
     public sealed class Initialize : XunitTestFramework
     {
         public Initialize(IMessageSink messageSink) : base(messageSink)
