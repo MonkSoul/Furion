c5f111c9b7e7f2e3804f4e34d0a60a9c17677df8
百小僧 <monksoul@outlook.com>
2021-07-29 09:17:09 +0800

    📁 通过 Jenkins 自动化发布文档

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -140,7 +140,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#723cff" }}>6750 +</div>
+            <div style={{ color: "#723cff" }}>6800 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>869,097</div>
+            <div style={{ color: "#1fd898" }}>887,184</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
