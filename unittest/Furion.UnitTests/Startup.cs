11e10569a4997fa1608488f5a70e57492a10c8ac
百小僧 <monksoul@outlook.com>
2021-08-01 13:49:40 +0800

    😂 更新单元测试项目初始化类名

--- unittest/Furion.UnitTests/Startup.cs
+++ unittest/Furion.UnitTests/Startup.cs
@@ -4,16 +4,16 @@ using Xunit.Abstractions;
 using Xunit.Sdk;
 
 // 配置启动类类型
-[assembly: TestFramework("Furion.UnitTests.Initialize", "Furion.UnitTests")]
+[assembly: TestFramework("Furion.UnitTests.Startup", "Furion.UnitTests")]
 
 namespace Furion.UnitTests
 {
     /// <summary>
     /// 单元测试初始化类
     /// </summary>
-    public sealed class Initialize : XunitTestFramework
+    public sealed class Startup : XunitTestFramework
     {
-        public Initialize(IMessageSink messageSink) : base(messageSink)
+        public Startup(IMessageSink messageSink) : base(messageSink)
         {
             var services = Inject.Create();
             services.Build();
