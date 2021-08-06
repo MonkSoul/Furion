2862c46839532bc5e9f04a1d3fe15ccfeeae8795
Monk <monksoul@outlook.com>
2021-08-06 09:26:35 +0800

    📁 通过 Jenkins 自动化发布文档

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>952,258</div>
+            <div style={{ color: "#1fd898" }}>956,753</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
