403b809d6d9f1092c71e15a3e991986b4f7930b6
Monk <monksoul@outlook.com>
2021-08-13 17:38:16 +0800

    📁 通过 Jenkins 自动化发布文档

--- handbook/src/components/GiveMeStar.js
+++ handbook/src/components/GiveMeStar.js
@@ -1,27 +1,31 @@
-import React, { useEffect } from "react";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import React from "react";
 import "./GiveMeStar.css";
+import Typist from "react-typist";
 
 export default function GiveMeStar() {
   return (
     <div className="furion-give-me-star">
-      ⭐️ 如果你喜欢{" "}
-      <a href="https://gitee.com/dotnetchina/Furion" target="_blank">
-        Furion
-      </a>{" "}
-      ，可以在{" "}
-      <a href="https://gitee.com/dotnetchina/Furion" target="_blank">
-        Gitee
-      </a>{" "}
-      中给个
-      <a href="https://gitee.com/dotnetchina/Furion" target="_blank">
-        <img
-          className="furion-gitee-star"
-          src="https://gitee.com/dotnetchina/Furion/badge/star.svg?theme=gvp"
-          alt="star"
-        ></img>
-      </a>{" "}
-      ⭐️
+      <Typist
+        cursor={{ show: false, hideWhenDone: true, hideWhenDoneDelay: 0 }}
+      >
+        ⭐️ 如果你喜欢{" "}
+        <a href="https://gitee.com/dotnetchina/Furion" target="_blank">
+          Furion
+        </a>{" "}
+        ，可以在{" "}
+        <a href="https://gitee.com/dotnetchina/Furion" target="_blank">
+          Gitee
+        </a>{" "}
+        中给个
+        <a href="https://gitee.com/dotnetchina/Furion" target="_blank">
+          <img
+            className="furion-gitee-star"
+            src="https://gitee.com/dotnetchina/Furion/badge/star.svg?theme=gvp"
+            alt="star"
+          ></img>
+        </a>{" "}
+        ⭐️
+      </Typist>
     </div>
   );
 }
