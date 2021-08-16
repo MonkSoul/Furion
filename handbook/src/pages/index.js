f805ff9d672d72563ea5fbcf545494fe9edda939
百小僧 <monksoul@outlook.com>
2021-08-16 14:07:26 +0000

    💢 更新 Nuget 下载量

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>983,515</div>
+            <div style={{ color: "#1fd898" }}>985,038</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
