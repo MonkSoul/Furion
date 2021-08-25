3a7714e5d482f4ed2bef4438707d8faf9dbb5c28
百小僧 <monksoul@outlook.com>
2021-08-25 13:17:49 +0800

    😢 修复大小写文件夹目录问题

---
+++ framework/Furion.Pure/ViewEngine/Internal/Penetrates.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+using System.IO;
+
+namespace Furion.ViewEngine
+{
+    /// <summary>
+    /// 常量、公共方法配置类
+    /// </summary>
+    internal static class Penetrates
+    {
+        /// <summary>
+        /// 模板保存目录
+        /// </summary>
+        internal static string TemplateSaveDir = "templates";
+
+        /// <summary>
+        /// 获取模板文件名
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        internal static string GetTemplateFileName(string fileName)
+        {
+            if (!Directory.Exists(TemplateSaveDir)) Directory.CreateDirectory(TemplateSaveDir);
+
+            if (!fileName.EndsWith(".dll", System.StringComparison.OrdinalIgnoreCase)) fileName += ".dll";
+            var templatePath = Path.Combine(TemplateSaveDir, "~" + fileName);
+
+            return templatePath;
+        }
+    }
+}
---
+++ framework/Furion/ViewEngine/Internal/ViewEnginePart.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+using Furion.DependencyInjection;
+using System;
+
+namespace Furion.ViewEngine
+{
+    /// <summary>
+    /// 字符串模板执行部件
+    /// </summary>
+    [SuppressSniffer]
+    public sealed partial class ViewEnginePart
+    {
+        /// <summary>
+        /// 静态缺省 视图 部件
+        /// </summary>
+        public static ViewEnginePart Default => new();
+
+        /// <summary>
+        /// 字符串模板
+        /// </summary>
+        public string Template { get; private set; }
+
+        /// <summary>
+        /// 视图配置选项
+        /// </summary>
+        public Action<IViewEngineOptionsBuilder> TemplateOptionsBuilder { get; private set; }
+
+        /// <summary>
+        /// 模型数据
+        /// </summary>
+        public (Type Type, object Model) TemplateModel { get; private set; } = (typeof(object), default);
+
+        /// <summary>
+        /// 模板缓存名称（不含拓展名）
+        /// </summary>
+        public string TemplateCachedFileName { get; private set; }
+
+        /// <summary>
+        /// 视图模板服务作用域
+        /// </summary>
+        public IServiceProvider ViewEngineScoped { get; private set; } = App.RootServices;
+    }
+}
