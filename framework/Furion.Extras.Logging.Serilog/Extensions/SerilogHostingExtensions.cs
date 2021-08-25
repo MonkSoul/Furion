c48254822c09092906ef77f04d54497e27665a92
百小僧 <monksoul@outlook.com>
2021-08-25 15:43:15 +0800

    🤔 修复 Worker Services 下日志不输出问题

--- framework/Furion.Extras.Logging.Serilog/Extensions/SerilogHostingExtensions.cs
+++ framework/Furion.Extras.Logging.Serilog/Extensions/SerilogHostingExtensions.cs
@@ -43,7 +43,7 @@ namespace Microsoft.Extensions.Hosting
                     if (hasWriteTo == null)
                     {
                         config.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
-                          .WriteTo.File(Path.Combine("logs", "application.log"), LogEventLevel.Information, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, encoding: Encoding.UTF8);
+                          .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "logs", "application.log"), LogEventLevel.Information, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, encoding: Encoding.UTF8);
                     }
                 }
             });
@@ -74,7 +74,7 @@ namespace Microsoft.Extensions.Hosting
                     if (hasWriteTo == null)
                     {
                         config.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
-                          .WriteTo.File(Path.Combine("logs", "application.log"), LogEventLevel.Information, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, encoding: Encoding.UTF8);
+                          .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "logs", "application.log"), LogEventLevel.Information, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, encoding: Encoding.UTF8);
                     }
                 }
             });
