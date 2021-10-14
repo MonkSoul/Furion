75785e42507b850e074fd886bb8c2235fe69836d
百小僧 <monksoul@outlook.com>
2021-10-14 19:09:45 +0800

    🐣 修复 v2.19 版本后模块化开发失效问题

--- framework/Furion.Pure/Reflection/Reflect.cs
+++ framework/Furion.Pure/Reflection/Reflect.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
 // Furion is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
@@ -46,7 +46,7 @@ namespace Furion.Reflection
         internal static Assembly LoadAssembly(string path)
         {
             if (!File.Exists(path)) return default;
-            return AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
+            return Assembly.LoadFrom(path);
         }
 
         /// <summary>
