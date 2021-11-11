82d270212c8bfbc9b095dd864e78a8f38e726f66
百小僧 <monksoul@outlook.com>
2021-11-11 15:03:45 +0800

    🎉 通过 Jenkins 自动发布文档

--- handbook/src/data/sponsor.js
+++ handbook/src/data/sponsor.js
@@ -3,6 +3,11 @@ const sponsors = [
         title: 'JNPF：基于代码生成器的 .NET 框架',
         picture: "img/jnpfsoft.png",
         url: "https://www.jnpfsoft.com/index.html?from=furion"
+    },
+    {
+        title: 'ProcessOn：在线流程图思维导图工具',
+        picture: "img/processon1.png",
+        url: "https://www.processon.com?from=furion"
     }
 ];
 
