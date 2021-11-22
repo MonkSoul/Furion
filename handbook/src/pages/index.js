a3198e0394c9cd8093b7f277ca157538e8ff7d80
百小僧 <monksoul@outlook.com>
2021-11-22 10:07:52 +0800

    🎨 Furion 在 Nuget 平台安装量破 136 万

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -164,7 +164,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>1,319,288</div>
+            <div style={{ color: "#1fd898" }}>1,363,185</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
