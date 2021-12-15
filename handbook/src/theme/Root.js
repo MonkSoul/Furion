e799342889748490f0a8e02541301abfa01b22cd
百小僧 <monksoul@outlook.com>
2021-12-15 11:32:49 +0800

    👱‍♀️ 修正文档错误，调整页面版面

--- handbook/src/theme/Root.js
+++ handbook/src/theme/Root.js
@@ -10,7 +10,7 @@ function Root({ children }) {
       <GiveMeStar />
       {/* <ZhiCai /> */}
       {children}
-      <JoinGroup />
+      {/* <JoinGroup /> */}
       {/* <StarProject /> */}
     </>
   );
--- handbook/src/theme/TOC/index.js
+++ handbook/src/theme/TOC/index.js
@@ -9,7 +9,6 @@ import clsx from 'clsx';
 import TOCItems from '@theme/TOCItems';
 import styles from './styles.module.css'; // Using a custom className
 // This prevents TOC highlighting to highlight TOCInline/TOCCollapsible by mistake
-import sponsors from '../../data/sponsor';
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
@@ -18,7 +17,7 @@ const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
 function TOC({ className, ...props }) {
   return (
     <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
-      {/* <Sponsor /> */}
+      <DotNETChina />
       <TOCItems
         {...props}
         linkClassName={LINK_CLASS_NAME}
@@ -28,16 +27,10 @@ function TOC({ className, ...props }) {
   );
 }
 
-function Sponsor() {
-  return <div style={{ margin: '0.5em', marginTop: '0', display: 'block', textAlign: 'center', borderBottom: '1px solid #dedede', paddingBottom: '0.2em' }}>
-    {sponsors.map(({ picture, url, title }, i) => <SponsorItem key={url} title={title} url={url} picture={picture} last={sponsors.length - 1 == i} />)}
-    <div><a href="mailto:monksoul@outlook.com" style={{ color: '#723cff', fontSize: 13, fontWeight: 'bold' }} title="monksoul@outlook.com">成为赞助商</a></div>
-  </div>
-}
-
-function SponsorItem({ picture, url, last, title }) {
-  return <a href={url} target="_blank" title={title} style={{ display: 'block', marginBottom: last ? null : '0.5em', position: 'relative' }}><img src={useBaseUrl(picture)} style={{ display: 'block', width: '100%' }} />
-    <span style={{ position: 'absolute', display: 'block', right: 0, bottom: 0, zIndex: 10, fontSize: 12, backgroundColor: 'rgba(0,0,0,0.8)', padding: '0 5px' }}>赞助商广告</span>
+function DotNETChina() {
+  return <a href='https://gitee.com/dotnetchina' target="_blank" style={{ display: 'block', position: 'relative' }} title='了解 dotNET China 组织'>
+    <img src={useBaseUrl("img/chinadotnet.png")} style={{ display: 'block', width: '90%', margin: '0 auto' }} />
   </a>
 }
+
 export default TOC;
