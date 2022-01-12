1b1d8a024bf45352e0c4ea423e017b3c2d71125e
百小僧 <monksoul@outlook.com>
2022-01-12 10:01:24 +0800

    🎨 更新2022年1月收到的捐赠 743元，合计总捐赠 14.8w

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -169,7 +169,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>1,759,866</div>
+            <div style={{ color: "#1fd898" }}>1,801,948</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
