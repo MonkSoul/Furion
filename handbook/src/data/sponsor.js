b3ab510ffb639c45ae5119bdc706102be8e17c44
MonkSoul <monksoul@outlook.com>
2022-04-28 16:56:11 +0800

    😎 更新文档内容，修正一些错别字

--- handbook/src/data/sponsor.js
+++ handbook/src/data/sponsor.js
@@ -1,14 +1,19 @@
 const sponsors = [
-    {
-        title: 'JNPF：基于代码生成器的 .NET 框架',
-        picture: "img/jnpfsoft.png",
-        url: "https://dotnet.jnpfsoft.com/login?from=furion"
-    },
-    {
-        title: 'ProcessOn：在线流程图思维导图工具',
-        picture: "img/processon1.png",
-        url: "https://www.processon.com?from=furion"
-    }
+  {
+    title: "JNPF：基于代码生成器的 .NET 框架",
+    picture: "img/jnpfsoft.png",
+    url: "https://dotnet.jnpfsoft.com/login?from=furion",
+  },
+  {
+    title: "ProcessOn：在线流程图思维导图工具",
+    picture: "img/processon1.png",
+    url: "https://www.processon.com?from=furion",
+  },
+  {
+    title: "Rust 语言圣经",
+    picture: "img/rust.png",
+    url: "https://course.rs/",
+  },
 ];
 
 export default sponsors;
