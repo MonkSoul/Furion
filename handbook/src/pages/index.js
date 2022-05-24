ca45b6a2e63522a6cd99e984c32212ef5f610525
MonkSoul <monksoul@outlook.com>
2022-05-24 11:36:57 +0800

    😢 完善客户端授权 axios 配置文档

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -173,7 +173,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,447,525</div>
+            <div style={{ color: "#1fd898" }}>2,461,034</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
