fa3d34fcf33ec2739fbebb4e67ad810286cc77e1
MonkSoul <monksoul@outlook.com>
2022-06-09 10:08:51 +0800

    😮 更新 Nuget 总安装量 255万次

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -173,7 +173,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,534,224</div>
+            <div style={{ color: "#1fd898" }}>2,553,878</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
