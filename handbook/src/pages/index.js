5d8d4093963ac1811ebf14dc2f59c22fa6a08b06
MonkSoul <monksoul@outlook.com>
2022-06-16 16:25:33 +0800

    😁 发布 v3.5.5 版本，修复 5 个隐藏极深的 bug

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -85,7 +85,7 @@ function Banner() {
             >
               我准备好了
             </Link>
-            <div className="furion-version">v3.5.4</div>
+            <div className="furion-version">v3.5.5</div>
           </div>
         </div>
         <div className="furion-banner-item">
@@ -174,7 +174,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,616,426</div>
+            <div style={{ color: "#1fd898" }}>2,618,147</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
