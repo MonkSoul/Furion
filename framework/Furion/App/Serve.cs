95cac5b391b70d73bfc94147ee40eef529b2eec6
MonkSoul <monksoul@outlook.com>
2022-06-17 18:38:32 +0800

    😁 优化 `Serve` 迷你 Web 主机，提供更多可能性

--- framework/Furion/App/Serve.cs
+++ framework/Furion/App/Serve.cs
@@ -14,22 +14,23 @@ using Microsoft.Extensions.Hosting;
 namespace System;
 
 /// <summary>
-/// 主机服务静态类
+/// 迷你 Web 主机静态类
 /// </summary>
 public static class Serve
 {
     /// <summary>
-    /// 启动一个轻量级迷你主机
+    /// 启动默认迷你 Web 主机
     /// </summary>
     /// <param name="url">默认 5000/5001 端口</param>
     public static void Run(string url = default)
     {
-        Host.CreateDefaultBuilder()
+        Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
             .ConfigureWebHostDefaults(webHostBuilder =>
             {
                 webHostBuilder.Inject()
                               .ConfigureServices((context, services) =>
                               {
+                                  // 默认配置控制器、动态 WebAPI 和规范化结果
                                   services.AddCorsAccessor();
                                   services.AddControllers().AddInjectWithUnifyResult();
                               })
@@ -68,5 +69,32 @@ public static class Serve
             })
             .Build()
             .Run();
+    }
+
+    /// <summary>
+    /// 启动自定义迷你 Web 主机
+    /// </summary>
+    /// <param name="webHostBuilderConfigure">Web 主机更多配置</param>
+    /// <param name="url">默认 5000/5001 端口</param>
+    /// <param name="hostBuilderConfigure">泛型主机更多配置</param>
+    public static void Run(Action<IWebHostBuilder> webHostBuilderConfigure, string url = default, Action<IHostBuilder> hostBuilderConfigure = default)
+    {
+        var hostBuilder = Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+              .ConfigureWebHostDefaults(webHostBuilder =>
+              {
+                  webHostBuilder.Inject();
+
+                  // 自定义端口
+                  if (!string.IsNullOrWhiteSpace(url))
+                  {
+                      webHostBuilder.UseUrls(url);
+                  }
+
+                  webHostBuilderConfigure(webHostBuilder);
+              });
+
+        hostBuilderConfigure?.Invoke(hostBuilder);
+
+        hostBuilder.Build().Run();
     }
 }
