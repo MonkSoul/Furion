968344aa07348f4c06f914abf7b6f46174633d81
MonkSoul <monksoul@outlook.com>
2022-06-20 19:28:43 +0800

    🤐 修复 `IHostService` 类型不能自动注册问题，之前只扫描了 `BackgroundService` 派生类

--- framework/Furion.Pure/App/Extensions/AppServiceCollectionExtensions.cs
+++ framework/Furion.Pure/App/Extensions/AppServiceCollectionExtensions.cs
@@ -150,7 +150,7 @@ public static class AppServiceCollectionExtensions
     public static IServiceCollection AddAppHostedService(this IServiceCollection services)
     {
         // 获取所有 BackgroundService 类型
-        var backgroundServiceTypes = App.EffectiveTypes.Where(u => typeof(BackgroundService).IsAssignableFrom(u) && u.Name != "GenericWebHostService");
+        var backgroundServiceTypes = App.EffectiveTypes.Where(u => typeof(IHostedService).IsAssignableFrom(u) && u.Name != "GenericWebHostService");
         var addHostServiceMethod = typeof(ServiceCollectionHostedServiceExtensions).GetMethods(BindingFlags.Static | BindingFlags.Public)
                             .Where(u => u.Name.Equals("AddHostedService") && u.IsGenericMethod && u.GetParameters().Length == 1)
                             .FirstOrDefault();
