dd55ff319fe3b10306fb9192b6192d12f24c4bd5
MonkSoul <monksoul@outlook.com>
2022-06-23 23:14:20 +0800

    😁 发布 v3.6.8 版本，主要解决单文件输出日志问题 #I5DQ2B

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -37,27 +37,3 @@ for ($i = 0; $i -le $framework_nupkgs.Length - 1; $i++){
 
 # 回到项目根目录
 cd ../../;
-
-Write-Warning "正在发布 templates 目录 Nuget 包......";
-
-# 查找 .\templates 下所有目录
-cd .\templates;
-$template_nupkgs = Get-ChildItem -Filter *.nupkg;
-
-# 遍历所有 *.nupkg 文件
-for ($i = 0; $i -le $template_nupkgs.Length - 1; $i++){
-    $item = $template_nupkgs[$i];
-
-    $nupkg = $item.FullName;
-
-    Write-Output "-----------------";
-    $nupkg;
-
-    # 发布到 nuget.org 平台
-    dotnet nuget push $nupkg --api-key $apikey --source https://api.nuget.org/v3/index.json;
-
-    Write-Output "-----------------";
-}
-
-# 回到项目根目录
-cd ../;
---
+++ templates/nuget-push.ps1
@@ -0,0 +1,27 @@
+﻿# 定义参数
+Param(
+    # Nuget APIKey
+    [string] $apikey
+)
+
+Write-Warning "正在发布 templates 目录 Nuget 包......";
+
+# 查找所有 *.nupkg 文件
+$template_nupkgs = Get-ChildItem -Filter *.nupkg;
+
+# 遍历所有 *.nupkg 文件
+for ($i = 0; $i -le $template_nupkgs.Length - 1; $i++){
+    $item = $template_nupkgs[$i];
+
+    $nupkg = $item.FullName;
+
+    Write-Output "-----------------";
+    $nupkg;
+
+    # 发布到 nuget.org 平台
+    dotnet nuget push $nupkg --api-key $apikey --source https://api.nuget.org/v3/index.json;
+
+    Write-Output "-----------------";
+}
+
+Write-Warning "发布成功";
