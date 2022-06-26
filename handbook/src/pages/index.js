eb7db40a985a43d5ee4ded40f501885f3d456313
MonkSoul <monksoul@outlook.com>
2022-06-26 22:02:49 +0800

    😁 发布 v3.7.0 版本，改进跨域配置 * 新增 `Vue/React/Angular` 前端代码生成

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v3.6.9</span>
+              <span className="furion-version">v3.7.0</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,710,264</div>
+            <div style={{ color: "#1fd898" }}>2,711,219</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
