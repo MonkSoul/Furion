dbea5d6119945258b0b67d05107adbdf9f7fee6b
MonkSoul <monksoul@outlook.com>
2022-07-19 11:52:50 +0800

    😐 更新文档错误

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,815,822</div>
+            <div style={{ color: "#1fd898" }}>2,826,351</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
