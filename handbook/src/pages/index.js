d2b3de966472eff1bdebb0bbfb136d79708f3f63
MonkSoul <monksoul@outlook.com>
2022-07-22 15:15:11 +0800

    😢 发布 v3.8.9 版本，优化脱敏词典编写规则，支持 `|` 分隔符

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v3.8.8</span>
+              <span className="furion-version">v3.8.9</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,831,328</div>
+            <div style={{ color: "#1fd898" }}>2,839,663</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
