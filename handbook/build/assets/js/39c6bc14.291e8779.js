d51fb2b5fcf3e5874e2d0197ee5d034f91689c73
MonkSoul <monksoul@outlook.com>
2022-07-22 11:03:53 +0800

    😢 发布 v3.8.8 版本，重构/改进底层脱敏模块，应用层接口不变

---
+++ handbook/build/assets/js/39c6bc14.291e8779.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfurion=self.webpackChunkfurion||[]).push([[6158],{4469:function(u){u.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- tools/cli.ps1
+++ tools/cli.ps1
@@ -24,7 +24,7 @@ Param(
     #[string]$UseDatabaseNames
 )
 
-$FurTools = "Furion Tools v3.8.7";
+$FurTools = "Furion Tools v3.8.8";
 
 # 输出信息
 $copyright = @"
