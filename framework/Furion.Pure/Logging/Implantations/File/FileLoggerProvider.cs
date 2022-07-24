3f77818211f990a3d88a9dbefd68a0117652e3ad
MonkSoul <monksoul@outlook.com>
2022-07-24 11:56:23 +0800

    😐 同步 `Furion` 近期更新至 `Furion.Pure`

--- framework/Furion.Pure/Logging/Implantations/File/FileLoggerProvider.cs
+++ framework/Furion.Pure/Logging/Implantations/File/FileLoggerProvider.cs
@@ -176,7 +176,7 @@ public sealed class FileLoggerProvider : ILoggerProvider
 
         try
         {
-            // 设置 1.5秒的缓冲时间，避免还有日志消除没有完成写入文件中
+            // 设置 1.5秒的缓冲时间，避免还有日志消息没有完成写入文件中
             _processQueueTask.Wait(1500);
         }
         catch (TaskCanceledException) { }
