75af5412e305bf29f22cc37411a9d23a4722752c
MonkSoul <monksoul@outlook.com>
2022-07-26 15:30:09 +0800

    😁 发布 v4.0.2 版本，改进日志监听器功能，支持更强大灵活的配置

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.0.1</span>
+              <span className="furion-version">v4.0.2</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,848,495</div>
+            <div style={{ color: "#1fd898" }}>2,864,907</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
