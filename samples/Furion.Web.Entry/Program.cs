196269aae585bb2f1241c7fb5749ed37ac0ce769
MonkSoul <monksoul@outlook.com>
2022-07-30 00:06:41 +0800

    😁 添加修改微软默认控制台日志模板文档 #I5JJJH

--- samples/Furion.Web.Entry/Program.cs
+++ samples/Furion.Web.Entry/Program.cs
@@ -1,1 +1,34 @@
-Serve.Run(RunOptions.Default);
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+
+Serve.Run(RunOptions.Default.ConfigureBuilder(builder =>
+{
+    builder.Logging.ClearProviders();
+    builder.Logging.AddConsole(options =>
+    {
+        options.FormatterName = "custom_format";
+    }).AddConsoleFormatter<YourForamt, ConsoleFormatterOptions>();
+}));
+
+public class YourForamt : ConsoleFormatter
+{
+    public YourForamt() : base("custom_format")
+    {
+    }
+
+    public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider scopeProvider, TextWriter textWriter)
+    {
+        string message = logEntry.Formatter?.Invoke(logEntry.State, logEntry.Exception);
+
+        if (message is null)
+        {
+            return;
+        }
+
+        textWriter.Write("日志级别：" + logEntry.LogLevel + " ");
+        textWriter.Write("其他叉叉：" + logEntry.Category + " ");
+
+        textWriter.Write(message);
+        textWriter.WriteLine();
+    }
+}
