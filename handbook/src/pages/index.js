39ee343c8105386087ee1ee282269e4400a5550d
MonkSoul <monksoul@outlook.com>
2022-08-02 09:33:29 +0800

    💖 完善 `Serve.Run()` 和 `Serve.Run(RunOptions.Default)` 说明

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,924,014</div>
+            <div style={{ color: "#1fd898" }}>2,926,552</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
