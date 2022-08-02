b56c7f41069b501959d6ba634b8034ecd960b84e
MonkSoul <monksoul@outlook.com>
2022-08-02 11:46:34 +0800

    😁 发布 v4.1.7 版本，改进自定义第三方 `Swagger UI` 兼容问题

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.1.6</span>
+              <span className="furion-version">v4.1.7</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>2,926,552</div>
+            <div style={{ color: "#1fd898" }}>2,937,757</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
--- tools/cli.ps1
+++ tools/cli.ps1
@@ -24,7 +24,7 @@ Param(
     #[string]$UseDatabaseNames
 )
 
-$FurTools = "Furion Tools v4.1.6";
+$FurTools = "Furion Tools v4.1.7";
 
 # 输出信息
 $copyright = @"
