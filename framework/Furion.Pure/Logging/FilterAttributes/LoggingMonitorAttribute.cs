ddfcd3391520ff9b5779e2f63324c2e80b5d0bc4
MonkSoul <monksoul@outlook.com>
2022-08-03 11:36:36 +0800

    😁 发布 v4.1.9 版本，修正 `LoggingMonitor` 输出不存在参数异常问题 #I5KC5P

--- framework/Furion.Pure/Logging/FilterAttributes/LoggingMonitorAttribute.cs
+++ framework/Furion.Pure/Logging/FilterAttributes/LoggingMonitorAttribute.cs
@@ -299,7 +299,7 @@ public sealed class LoggingMonitorAttribute : Attribute, IAsyncActionFilter
         foreach (var parameter in parameters)
         {
             var name = parameter.Name;
-            parameterValues.TryGetValue(name, out var value);
+            _ = parameterValues.TryGetValue(name, out var value);
             var type = parameter.ParameterType;
 
             object rawValue = default;
