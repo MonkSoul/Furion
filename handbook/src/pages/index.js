da782f064ffdeecfde3f2ef279289dee2d95b911
MonkSoul <monksoul@outlook.com>
2022-08-19 10:56:53 +0800

    🥲 新增依赖注入在非 `Web` 或多线程中解析服务文档

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>3,012,300</div>
+            <div style={{ color: "#1fd898" }}>3,021,480</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
