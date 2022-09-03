2ef31b0702049374acb0291377241193a90c8e85
MonkSoul <monksoul@outlook.com>
2022-09-03 09:56:47 +0800

    😊 发布 v4.3.9 版本，改善 `LoggingMonitor` 功能 #I5PENW #I5PEPA #I5PERE #I5PFJ1 #I5PFOW #I5PFQ2 !547 !548 !549
    - 含 #I5PENW:HttpRequestMessage 拓展中追加查询参数时的空引用异常 功能发布
    - 含 #I5PEPA:📝 `LoggingMonitor` 支持添加更多自定义配置 功能发布
    - 含 #I5PERE: LoggingMonitor 能否支持自定义插入参数和值 功能发布
    - 含 #I5PFJ1:📝 `LoggingMonitor` 和日志功能改进 功能发布
    - 含 #I5PFOW:📝监听日志记录的接口返回值可配置是否记录或截断 功能发布
    - 含 #I5PFQ2:日志过滤器/筛选器 根据等级筛选失效 功能发布
    - 更新示例项目 samples 依赖至 v4.3.9 版本
    - 发布 v4.3.9 版本文档
    - Replit 网站 Furion 案例同步到 v4.3.9 版本
    - 同步更新日志

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.3.8</span>
+              <span className="furion-version">v4.3.9</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>3,102,222</div>
+            <div style={{ color: "#1fd898" }}>3,109,291</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
