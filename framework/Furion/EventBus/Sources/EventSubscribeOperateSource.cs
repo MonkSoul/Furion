3e45020eca5948d18d5cb405a665aae44088fd20
MonkSoul <monksoul@outlook.com>
2022-09-20 00:16:24 +0800

    😐 修复基于 `Redis` 重写事件存储器序列化 `IEventSource` 实例异常问题

--- framework/Furion/EventBus/Sources/EventSubscribeOperateSource.cs
+++ framework/Furion/EventBus/Sources/EventSubscribeOperateSource.cs
@@ -40,15 +40,17 @@ internal sealed class EventSubscribeOperateSource : IEventSource
     public object Payload { get; set; }
 
     /// <summary>
-    /// 取消任务 Token
-    /// </summary>
-    /// <remarks>用于取消本次消息处理</remarks>
-    public CancellationToken CancellationToken { get; set; }
-
-    /// <summary>
     /// 事件创建时间
     /// </summary>
     public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    /// 取消任务 Token
+    /// </summary>
+    /// <remarks>用于取消本次消息处理</remarks>
+    [Newtonsoft.Json.JsonIgnore]
+    [System.Text.Json.Serialization.JsonIgnore]
+    public CancellationToken CancellationToken { get; set; }
 
     /// <summary>
     /// 事件处理程序
