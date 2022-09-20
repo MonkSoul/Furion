0b4ceedd215abded3927100f083bc3e8b3d927ed
MonkSoul <monksoul@outlook.com>
2022-09-20 16:47:54 +0800

    😊 添加事件总线 `Kafka` 实现例子，感谢 @visker #I5P5UG

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>3,398,026</div>
+            <div style={{ color: "#1fd898" }}>3,400,096</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
