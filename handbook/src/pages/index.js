ad5b4bf8408ae62d68ecdbbc6c74ecefc553f160
MonkSoul <monksoul@outlook.com>
2022-09-23 11:45:36 +0800

    😊 发布 v4.4.9 版本，开放底层 `Inject` 权限
    - 包含 #I4PQ2Y:无法重写ConfigureAppConfiguration配置 功能更新
    - 修复集成第三方配置中心时获取的不是最新数据问题 2cdef6b
    - 开放底层 Inject 配置功能，使第三方模块更容易集成 1182283
    - 新增集成第三方配置中心文档
    - 新增第三方事件总线和 Furion 集成文档
    - 新增使用第三方 Swagger UI 集成文档
    - 发布 v4.4.9 版本文档
    - 更新示例项目 samples 依赖至 v4.4.9 版本
    - Replit 网站 Furion 案例同步到 v4.4.9 版本
    - Gitee 和 Github 发布 Release-v4.4.9 版本
    - 同步更新日志

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.4.8</span>
+              <span className="furion-version">v4.4.9</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>3,406,995</div>
+            <div style={{ color: "#1fd898" }}>3,409,622</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
