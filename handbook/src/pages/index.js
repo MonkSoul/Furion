92dcc62d593fe49eb28402c3756ca4801e082908
MonkSoul <monksoul@outlook.com>
2022-10-12 12:40:08 +0800

    😊 发布 v4.6.3 版本，改进远程请求和数据库连接
    - 修复远程请求不能在 Worker Serivce 中进行构造函数注入，原因是注册为 Scope 范围作用域 4.6.3 974f835
    - 个别服务器的 SQL Server 不支持 TLS 1.2 协议问题 4.6.3 974f835
    - 发布 v4.6.3 版本文档
    - 更新示例项目 samples 依赖至 v4.6.3 版本
    - Replit 网站 Furion 案例同步到 v4.6.3 版本
    - Gitee 和 Github 发布 Release-v4.6.3 版本
    - 同步更新日志

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.6.2</span>
+              <span className="furion-version">v4.6.3</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -373,8 +373,8 @@ function WhoUse() {
           <div></div>
           <h1>他们都在用</h1>
           <p>
-            Furion 已在全球超过 370万
-            个项目中使用。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
+            Furion
+            已在全球超过370万个项目中使用。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
           </p>
           <a
             className="furion-get-start"
