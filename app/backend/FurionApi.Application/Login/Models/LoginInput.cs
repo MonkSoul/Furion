32c449e608e6a4ceaa07584f1771f8d9917c43b6
MonkSoul <monksoul@outlook.com>
2022-10-18 17:27:44 +0800

    😊 `Furion` 通用后台：添加登录接口伪代码

---
+++ app/backend/FurionApi.Application/Login/Models/LoginInput.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurionApi.Application;
+
+public class LoginInput
+{
+    [Required]
+    public string UserName { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    public bool RememberMe { get; set; }
+}
---
+++ app/backend/FurionApi.Application/Login/Models/PublicUser.cs
@@ -0,0 +1,10 @@
+﻿namespace FurionApi.Application;
+
+/// <summary>
+/// 公开的用户信息
+/// </summary>
+public class PublicUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
