2de8d00f8da3eead65dd4baf3669adf923d098cc
董红鹏 <donghp@bibibi.net>
2022-11-10 16:26:01 +0800

    增加DateOnly、DateOnly?、TimeOnly、TimeOnly?类型Json转换拓展

---
+++ framework/Furion.Pure/JsonSerialization/Converters/SystemTextJson/SystemTextJsonDateOnlyJsonConverter.cs
@@ -0,0 +1,134 @@
+﻿// MIT License
+//
+// Copyright (c) 2020-2022 百小僧, Baiqian Co.,Ltd and Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#if !NET5_0
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Furion.JsonSerialization;
+
+
+/// <summary>
+/// DateOnly 类型序列化
+/// </summary>
+[SuppressSniffer]
+public class SystemTextJsonDateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    /// <summary>
+    /// 默认构造函数
+    /// </summary>
+    public SystemTextJsonDateOnlyJsonConverter()
+    {
+        Format ??= "yyyy-MM-dd";
+    }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="format"></param>
+    public SystemTextJsonDateOnlyJsonConverter(string format)
+    {
+        Format = format;
+    }
+
+    /// <summary>
+    /// 时间格式化格式
+    /// </summary>
+    public string Format { get; private set; }
+
+    /// <summary>
+    /// 反序列化
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.Parse(reader.GetString());
+    }
+
+    /// <summary>
+    /// 序列化
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(Format));
+    }
+}
+
+/// <summary>
+/// DateOnly? 类型序列化
+/// </summary>
+[SuppressSniffer]
+public class SystemTextJsonNullableDateOnlyJsonConverter : JsonConverter<DateOnly?>
+{
+    /// <summary>
+    /// 默认构造函数
+    /// </summary>
+    public SystemTextJsonNullableDateOnlyJsonConverter()
+    {
+        Format ??= "yyyy-MM-dd";
+    }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="format"></param>
+    public SystemTextJsonNullableDateOnlyJsonConverter(string format)
+    {
+        Format = format;
+    }
+
+    /// <summary>
+    /// 时间格式化格式
+    /// </summary>
+    public string Format { get; private set; }
+
+    /// <summary>
+    /// 反序列化
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override DateOnly? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.TryParse(reader.GetString(), out DateOnly date) ? date : null;
+    }
+
+    /// <summary>
+    /// 序列化
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, DateOnly? value, JsonSerializerOptions options)
+    {
+        if (value == null) writer.WriteNullValue();
+        else writer.WriteStringValue(value.Value.ToString(Format));
+    }
+}
+#endif
