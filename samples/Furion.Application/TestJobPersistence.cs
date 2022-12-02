366cd03011fbea27c00bcb11e1d079c2cceced9c
MonkSoul <monksoul@outlook.com>
2022-12-02 16:46:41 +0800

    😢 修正 `samples` 编译异常代码

--- samples/Furion.Application/TestJobPersistence.cs
+++ samples/Furion.Application/TestJobPersistence.cs
@@ -19,24 +19,14 @@ public class TestJobPersistence : IJobPersistence
 
     public SchedulerBuilder OnLoading(SchedulerBuilder builder)
     {
-        builder.GetJobBuilder().SetDescription("测试作业描述");
-        builder.GetTriggerBuilders().ForEach(b =>
-        {
-            b.SetDescription("测试触发器描述");
-        });
-
-        _logger.LogInformation(builder.ConvertToJSON());
-
         return builder;
     }
 
     public void OnChanged(PersistenceContext context)
     {
-        _logger.LogInformation("{behavior}：{jobId} {updatedTime}", context.Behavior, context.JobId, context.JobDetail.UpdatedTime);
     }
 
     public void OnTriggerChanged(PersistenceTriggerContext context)
     {
-        _logger.LogInformation("{behavior}：{jobId} {triggerId} {status} {updatedTime}", context.Behavior, context.JobId, context.TriggerId, context.Trigger.Status, context.Trigger.UpdatedTime);
     }
 }
