88af249c0b43822a91c20cbce6196f3118aebd42
MonkSoul <monksoul@outlook.com>
2022-12-08 00:18:44 +0800

    😊 发布 v4.8.2.13 版本，无功能新增修复，底层优化改进

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -84,7 +84,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.8.2.12</span>
+              <span className="furion-version">v4.8.2.13</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>5,053,706</div>
+            <div style={{ color: "#1fd898" }}>5,055,836</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
