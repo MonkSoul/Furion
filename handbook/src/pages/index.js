19b6868f410239d3f751ca1e2474f16b177fd195
MonkSoul <monksoul@outlook.com>
2022-12-23 15:11:37 +0800

    🙂 改进多语言 `L.SetCulture` 方法，可配置运行时多语言立即有效 #I66JWA

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>5,303,814</div>
+            <div style={{ color: "#1fd898" }}>5,308,920</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
