b6b05f20a054e243d6d99b682daae03504b5b57e
MonkSoul <monksoul@outlook.com>
2023-01-03 12:18:00 +0800

    😊 发布 v4.8.4.3 版本，新增获取作业触发器运行记录功能

--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
           position: "left",
           items: [
             {
-              label: "📝 查看日志（v4.8.4.2）",
+              label: "📝 查看日志（v4.8.4.3）",
               href: "/docs/upgrade",
             },
             {
