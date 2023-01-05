03b9c0c100d495686774f613364795d4ae4bd71f
MonkSoul <monksoul@outlook.com>
2023-01-05 11:40:28 +0800

    😊 发布 v4.8.4.7 版本，压缩定时任务大小，由 `114KB` 减少至 `61KB`

--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
           position: "left",
           items: [
             {
-              label: "📝 查看日志（v4.8.4.6）",
+              label: "📝 查看日志（v4.8.4.7）",
               href: "/docs/upgrade",
             },
             {
--- tools/cli.ps1
+++ tools/cli.ps1
@@ -24,7 +24,7 @@ Param(
     #[string]$UseDatabaseNames
 )
 
-$FurTools = "Furion Tools v4.8.4.6";
+$FurTools = "Furion Tools v4.8.4.7";
 
 # 输出信息
 $copyright = @"
