9cffda078c1bf9a4575e3609b9769ce66270ee33
MonkSoul <monksoul@outlook.com>
2023-01-11 18:15:30 +0800

    😊 发布 v4.8.3.13 版本，适配 `.NET7.0.2/6.0.13`，改进 `Swagger UI`

--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
           position: "left",
           items: [
             {
-              label: "📝 查看日志（v4.8.4.12）",
+              label: "📝 查看日志（v4.8.4.13）",
               href: "/docs/upgrade",
             },
             {
--- tools/cli.ps1
+++ tools/cli.ps1
@@ -24,7 +24,7 @@ Param(
     #[string]$UseDatabaseNames
 )
 
-$FurTools = "Furion Tools v4.8.4.12";
+$FurTools = "Furion Tools v4.8.4.13";
 
 # 输出信息
 $copyright = @"
