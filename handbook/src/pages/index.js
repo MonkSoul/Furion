00f1aca1fdd0c8f34af909468a083628871928a8
MonkSoul <monksoul@outlook.com>
2023-02-09 13:44:02 +0800

    😐 更新虚拟文件文档，添加文件下载说明 #I6DXIX

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -183,7 +183,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>6,063,288</div>
+            <div style={{ color: "#1fd898" }}>6,069,046</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
