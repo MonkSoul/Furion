9a736dab03233c68a5cd4dc001609b5ad19139c7
MonkSoul <monksoul@outlook.com>
2023-02-11 18:40:21 +0800

    😊 发布新文档，完善全球化/多语言切换方式

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -190,7 +190,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>6,076,898</div>
+            <div style={{ color: "#1fd898" }}>6,079,471</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
