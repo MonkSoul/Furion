dcc88f52937bd912389c44782f86f12328acb38e
MonkSoul <monksoul@outlook.com>
2023-02-18 12:36:58 +0800

    😊 发布 v4.8.6.6 版本，修复粘土对象不支持中文键问题

---
+++ handbook/build/assets/js/2b0adb68.48b6d44a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfurion=self.webpackChunkfurion||[]).push([[3794],{4469:function(u){u.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -75,7 +75,7 @@ module.exports = {
           position: "left",
           items: [
             {
-              label: "📝 查看日志（v4.8.6.5）",
+              label: "📝 查看日志（v4.8.6.6）",
               href: "/docs/upgrade",
             },
             {
