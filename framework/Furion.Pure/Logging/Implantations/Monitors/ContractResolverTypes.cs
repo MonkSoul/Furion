4f9d3d8d4d02f4dc9fd5a39641fd2dc4e4e8bee7
MonkSoul <monksoul@outlook.com>
2023-02-21 18:54:14 +0800

    😐 新增审计日志 `LoggingMonitor` 支持配置序列化属性命名规则 #I6GPUP

--- framework/Furion.Pure/Logging/Implantations/Monitors/ContractResolverTypes.cs
+++ framework/Furion.Pure/Logging/Implantations/Monitors/ContractResolverTypes.cs
@@ -23,51 +23,19 @@
 namespace Furion.Logging;
 
 /// <summary>
-/// LoggingMonitor 方法配置
+/// LoggingMonitor 序列化属性命名规则选项
 /// </summary>
 [SuppressSniffer]
-public sealed class LoggingMonitorMethod
+public enum ContractResolverTypes
 {
     /// <summary>
-    /// 方法名称
+    /// CamelCase 小驼峰
     /// </summary>
-    /// <remarks>完全限定名</remarks>
-    public string FullName { get; set; }
+    /// <remarks>默认值</remarks>
+    CamelCase = 0,
 
     /// <summary>
-    /// 是否记录返回值
+    /// 保持原样
     /// </summary>
-    /// <remarks>bool 类型，默认输出</remarks>
-    public bool WithReturnValue { get; set; } = true;
-
-    /// <summary>
-    /// 设置返回值阈值
-    /// </summary>
-    /// <remarks>配置返回值字符串阈值，超过这个阈值将截断，默认全量输出</remarks>
-    public int ReturnValueThreshold { get; set; } = 0;
-
-    /// <summary>
-    /// 配置 Json 输出行为
-    /// </summary>
-    public JsonBehavior JsonBehavior { get; set; } = JsonBehavior.None;
-
-    /// <summary>
-    /// 配置序列化忽略的属性名称
-    /// </summary>
-    public string[] IgnorePropertyNames { get; set; }
-
-    /// <summary>
-    /// 配置序列化忽略的属性类型
-    /// </summary>
-    public Type[] IgnorePropertyTypes { get; set; }
-
-    /// <summary>
-    /// JSON 输出格式化
-    /// </summary>
-    public bool JsonIndented { get; set; } = false;
-
-    /// <summary>
-    /// 序列化属性命名规则（返回值）
-    /// </summary>
-    public ContractResolverTypes ContractResolver { get; set; } = ContractResolverTypes.CamelCase;
+    Default = 1
 }
