22605d037f4dd679ebc5cd6e58cc842d63789211
MonkSoul <monksoul@outlook.com>
2023-02-22 13:04:20 +0800

    😊 发布 v4.8.7 版本，完成框架 `.NET8` 所有功能适配

--- handbook/docusaurus.config.js
+++ handbook/docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
     announcementBar: {
       id: "v4.8.7",
       content:
-        "🚀 Furion v4.8.7 已正式发布（适配 .NET8），浏览 <a href='/docs/upgrade'>[更新日志]</a> 或查看 <a href='https://gitee.com/dotnetchina/Furion/issues/I6DQ57' target='_blank'>[本期更新]</a>",
+        "🚀 Furion v4.8.7 已正式发布（适配 .NET8），浏览 <a href='/docs/upgrade'>[更新日志]</a> 或查看 <a href='https://gitee.com/dotnetchina/Furion/issues/I6GVN8' target='_blank'>[本期更新]</a>",
       backgroundColor: "#723cff",
       textColor: "yellow",
       isCloseable: true,
