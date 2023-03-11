681642f72ec7374c4ced8ebf57418f9c8371a701
MonkSoul <monksoul@outlook.com>
2023-03-11 13:25:21 +0800

    😊 发布 v4.8.7.7 版本，新增定时任务 `HTTP` 作业

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -91,7 +91,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.8.7.6</span>
+              <span className="furion-version">v4.8.7.7</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -190,7 +190,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>6,904,814</div>
+            <div style={{ color: "#1fd898" }}>6,907,106</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
