ba6230a4d50814c37b3800d34e948b1d76bb5480
MonkSoul <monksoul@outlook.com>
2023-03-13 10:42:12 +0800

    🙄 修正代码注释和文档纰漏地方

--- framework/Furion.Pure/Logging/Implantations/Database/DatabaseLoggerProvider.cs
+++ framework/Furion.Pure/Logging/Implantations/Database/DatabaseLoggerProvider.cs
@@ -112,7 +112,7 @@ public sealed class DatabaseLoggerProvider : ILoggerProvider, ISupportExternalSc
 
         try
         {
-            // 设置 1.5秒的缓冲时间，避免还有日志消息没有完成写入文件中
+            // 设置 1.5秒的缓冲时间，避免还有日志消息没有完成写入数据库中
             _processQueueTask?.Wait(1500);
         }
         catch (TaskCanceledException) { }
