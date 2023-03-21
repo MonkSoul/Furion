6be764f95cb022ff52fca39c55155d4eb0bdc051
MonkSoul <monksoul@outlook.com>
2023-03-21 18:02:26 +0800

    😊 发布 v4.8.7.18 版本，改进达梦数据库支持和定时任务优化

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -91,7 +91,7 @@ function Banner() {
           <div className="furion-get-start-btn">
             <Link className="furion-get-start" to={useBaseUrl("docs/serverun")}>
               入门指南
-              <span className="furion-version">v4.8.7.17</span>
+              <span className="furion-version">v4.8.7.18</span>
             </Link>
             <Link
               className="furion-try-demo"
