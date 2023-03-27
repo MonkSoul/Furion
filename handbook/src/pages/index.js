3fd6ee9e900c6c1622ea24712daabacd9ecd2981
MonkSoul <monksoul@outlook.com>
2023-03-27 16:25:48 +0800

    😊 发布 v4.8.7.22 版本，改进远程请求和规范化文档 #I6QMLR #I6QMUN #I6QM23

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -95,7 +95,7 @@ function Banner() {
               to={useBaseUrl("docs/category/getstart")}
             >
               入门指南
-              <span className="furion-version">v4.8.7.21</span>
+              <span className="furion-version">v4.8.7.22</span>
             </Link>
             <Link
               className="furion-try-demo"
