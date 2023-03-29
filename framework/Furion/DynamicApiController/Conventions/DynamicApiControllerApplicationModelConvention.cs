712e96a61e2b7fd0381f0aef53feb9be463b5ce2
MonkSoul <monksoul@outlook.com>
2023-03-29 13:54:55 +0800

    😐 修复 因 `4.8.7.22` 版本导致动态 `WebAPI` 类型注释丢失问题 #I6QM23

--- framework/Furion/DynamicApiController/Conventions/DynamicApiControllerApplicationModelConvention.cs
+++ framework/Furion/DynamicApiController/Conventions/DynamicApiControllerApplicationModelConvention.cs
@@ -73,12 +73,12 @@ internal sealed class DynamicApiControllerApplicationModelConvention : IApplicat
             // 解析 [ApiDescriptionSettings] 特性
             var controllerApiDescriptionSettings = controllerType.IsDefined(typeof(ApiDescriptionSettingsAttribute), true) ? controllerType.GetCustomAttribute<ApiDescriptionSettingsAttribute>(true) : default;
 
-            // 存储排序给 Swagger 使用
-            Penetrates.ControllerOrderCollection.TryAdd(controller.ControllerName, (controllerApiDescriptionSettings?.Tag ?? controller.ControllerName, controllerApiDescriptionSettings?.Order ?? 0));
-
             // 判断是否处理 Mvc控制器
             if (typeof(ControllerBase).IsAssignableFrom(controllerType))
             {
+                // 存储排序给 Swagger 使用
+                Penetrates.ControllerOrderCollection.TryAdd(controller.ControllerName, (controllerApiDescriptionSettings?.Tag ?? controller.ControllerName, controllerApiDescriptionSettings?.Order ?? 0));
+
                 if (!_dynamicApiControllerSettings.SupportedMvcController.Value || controller.ApiExplorer?.IsVisible == false)
                 {
                     // 控制器默认处理规范化结果
@@ -114,6 +114,9 @@ internal sealed class DynamicApiControllerApplicationModelConvention : IApplicat
 
         // 配置控制器路由特性
         ConfigureControllerRouteAttribute(controller, controllerApiDescriptionSettings);
+
+        // 存储排序给 Swagger 使用
+        Penetrates.ControllerOrderCollection.TryAdd(controller.ControllerName, (controllerApiDescriptionSettings?.Tag ?? controller.ControllerName, controllerApiDescriptionSettings?.Order ?? 0));
 
         var actions = controller.Actions;
 
