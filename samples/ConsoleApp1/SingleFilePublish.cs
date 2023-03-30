e96e02827bd5535f7c266b83391287e44a55d049
MonkSoul <monksoul@outlook.com>
2023-03-30 15:20:29 +0800

    😊 完成将 `Web` 项目生成桌面软件 `WinForm/WPF` 文档

---
+++ samples/ConsoleApp1/SingleFilePublish.cs
@@ -0,0 +1,36 @@
+﻿using Furion;
+using System.Reflection;
+
+namespace ConsoleApp1;
+
+/// <summary>
+/// 解决单文件发布问题
+/// </summary>
+public class SingleFilePublish : ISingleFilePublish
+{
+    /// <summary>
+    /// 解决单文件不能扫描的程序集
+    /// </summary>
+    /// <remarks>和 <see cref="IncludeAssemblyNames"/> 可同时配置</remarks>
+    /// <returns></returns>
+    public Assembly[] IncludeAssemblies()
+    {
+        return Array.Empty<Assembly>();
+    }
+
+    /// <summary>
+    /// 解决单文件不能扫描的程序集名称
+    /// </summary>
+    /// <remarks>和 <see cref="IncludeAssemblies"/> 可同时配置</remarks>
+    /// <returns></returns>
+    public string[] IncludeAssemblyNames()
+    {
+        return new[]
+        {
+            "Furion.Application",
+            "Furion.Core",
+            "Furion.EntityFramework.Core",
+            "Furion.Web.Core"
+        };
+    }
+}
