525d055699554e2491b606e6540509a1fa76974e
MonkSoul <monksoul@outlook.com>
2023-03-31 11:08:04 +0800

    😐 调整 多语言中间件 `app.UseAppLocalization()` 添加 `Action<options>` 委托参数 #I6RUOU

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -195,7 +195,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>7,302,907</div>
+            <div style={{ color: "#1fd898" }}>7,306,112</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
