593f46f10d067135258957e306219a148001ffc6
MonkSoul <monksoul@outlook.com>
2023-04-03 15:36:35 +0800

    😐 更新 `WinForm` 混合开发示例

--- samples/WinFormsApp1/Form1.cs
+++ samples/WinFormsApp1/Form1.cs
@@ -8,14 +8,8 @@ public partial class Form1 : Form
     public Form1(IServer server)    // 注入 IServer 服务，获取 Web 启动地址/端口
     {
         InitializeComponent();
-        Resize += Form1_Resize;
 
-        webview.Size = this.ClientSize;
+        webview.Dock = DockStyle.Fill;
         webview.Source = new Uri(server.GetServerAddress());
-    }
-
-    private void Form1_Resize(object? sender, EventArgs e)
-    {
-        webview.Size = this.ClientSize;
     }
 }
