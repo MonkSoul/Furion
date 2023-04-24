18c2a1c99dba093789e1abbe4b730d283b34a83b
MonkSoul <monksoul@outlook.com>
2023-04-24 22:23:43 +0800

    😊 新增 定时任务支持在非 `IOC/DI` 项目类型中使用 https://gitee.com/dotnetchina/Sundial/issues/

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -195,7 +195,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>8,156,811</div>
+            <div style={{ color: "#1fd898" }}>8,158,850</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
