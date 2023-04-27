7fed9dd9fece3d7452d578c0dda1573b30b70306
MonkSoul <monksoul@outlook.com>
2023-04-27 10:28:23 +0800

    😊 通过 `Jenkins` 发布新文档，改进文档体验

--- handbook/build/script/baidutongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?91e98d00c5024e8d87b0bb3c10281d59";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
