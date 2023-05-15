4590cb2542331c35ce0797af3a303069a9035ab9
MonkSoul <monksoul@outlook.com>
2023-05-15 18:34:12 +0800

    😐 修复 因 [9d8cb82](https://gitee.com/dotnetchina/Furion/commit/9d8cb82e4ce983839cf13c3c74640b08f258c325) 代码提交导致服务 `AOP` 服务异常拦截问题 #I73A8E

--- framework/Furion.Pure/DependencyInjection/Dependencies/IScoped.cs
+++ framework/Furion.Pure/DependencyInjection/Dependencies/IScoped.cs
@@ -12,8 +12,6 @@
 // 在任何情况下，作者或版权持有人都不对任何索赔、损害或其他责任负责，无论这些追责来自合同、侵权或其它行为中，
 // 还是产生于、源于或有关于本软件以及本软件的使用或其它处置。
 
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Furion.DependencyInjection;
 
 /// <summary>
@@ -21,8 +19,4 @@ namespace Furion.DependencyInjection;
 /// </summary>
 public interface IScoped : IPrivateDependency
 {
-    /// <summary>
-    /// 服务生存期
-    /// </summary>
-    ServiceLifetime IPrivateDependency.Lifetime => ServiceLifetime.Scoped;
 }
