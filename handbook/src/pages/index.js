789599c829183b48e0ad2e14baff769c5810f252
MonkSoul <monksoul@outlook.com>
2023-05-16 09:20:55 +0800

    😊 发布 `v4.8.8.18` 版本，改进动态 `WebAPI` 路由生成

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -93,7 +93,7 @@ function Banner() {
               to={useBaseUrl("docs/category/getstart")}
             >
               入门指南
-              <span className="furion-version">v4.8.8.17</span>
+              <span className="furion-version">v4.8.8.18</span>
             </Link>
             <Link
               className="furion-try-demo"
@@ -193,7 +193,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>8,406,930</div>
+            <div style={{ color: "#1fd898" }}>8,412,448</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
@@ -297,7 +297,7 @@ function WhoUse() {
           <h1>他们都在用</h1>
           <p>
             Furion 已在 NuGet
-            平台超840万次下载。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
+            平台超841万次下载。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
           </p>
           <a
             className="furion-get-start"
--- tools/cli.ps1
+++ tools/cli.ps1
@@ -24,7 +24,7 @@ Param(
     #[string]$UseDatabaseNames
 )
 
-$FurTools = "Furion Tools v4.8.8.17";
+$FurTools = "Furion Tools v4.8.8.18";
 
 # 输出信息
 $copyright = @"
