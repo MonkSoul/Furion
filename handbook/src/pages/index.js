bd5ca6951f14cc73a6776dd53fba33ee1e8c3d8e
MonkSoul <monksoul@outlook.com>
2023-05-18 03:18:47 +0800

    😊 发布 `v4.8.8.20` 版本，新增定时任务看板支持二级虚拟目录

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -93,7 +93,7 @@ function Banner() {
               to={useBaseUrl("docs/category/getstart")}
             >
               入门指南
-              <span className="furion-version">v4.8.8.19</span>
+              <span className="furion-version">v4.8.8.20</span>
             </Link>
             <Link
               className="furion-try-demo"
--- tools/cli.ps1
+++ tools/cli.ps1
@@ -24,7 +24,7 @@ Param(
     #[string]$UseDatabaseNames
 )
 
-$FurTools = "Furion Tools v4.8.8.19";
+$FurTools = "Furion Tools v4.8.8.20";
 
 # 输出信息
 $copyright = @"
