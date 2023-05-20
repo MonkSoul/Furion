93df80117ac31a8af4ee6e3352b2209f4e67c6bc
MonkSoul <monksoul@outlook.com>
2023-05-20 18:37:08 +0800

    😊 更新用户捐赠列表信息

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -193,7 +193,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>8,499,964</div>
+            <div style={{ color: "#1fd898" }}>8,539,502</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
@@ -297,7 +297,7 @@ function WhoUse() {
           <h1>他们都在用</h1>
           <p>
             Furion 已在 NuGet
-            平台超849万次下载。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
+            平台超853万次下载。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
           </p>
           <a
             className="furion-get-start"
