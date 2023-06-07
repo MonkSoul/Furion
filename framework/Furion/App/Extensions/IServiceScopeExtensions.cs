11a55e1796acae3318ea2c78cb3e88ba4d53c670
MonkSoul <monksoul@outlook.com>
2023-06-07 12:27:06 +0800

    😊 新增 `IServiceScope.CreateDefaultHttpContext` 拓展方法

---
+++ framework/Furion/App/Extensions/IServiceScopeExtensions.cs
@@ -0,0 +1,42 @@
+﻿// MIT 许可证
+//
+// 版权 © 2020-present 百小僧, 百签科技（广东）有限公司 和所有贡献者
+//
+// 特此免费授予任何获得本软件副本和相关文档文件（下称“软件”）的人不受限制地处置该软件的权利，
+// 包括不受限制地使用、复制、修改、合并、发布、分发、转授许可和/或出售该软件副本，
+// 以及再授权被配发了本软件的人如上的权利，须在下列条件下：
+//
+// 上述版权声明和本许可声明应包含在该软件的所有副本或实质成分中。
+//
+// 本软件是“如此”提供的，没有任何形式的明示或暗示的保证，包括但不限于对适销性、特定用途的适用性和不侵权的保证。
+// 在任何情况下，作者或版权持有人都不对任何索赔、损害或其他责任负责，无论这些追责来自合同、侵权或其它行为中，
+// 还是产生于、源于或有关于本软件以及本软件的使用或其它处置。
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using System.Security.Claims;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// <see cref="IServiceScope"/> 拓展类
+/// </summary>
+public static class IServiceScopeExtensions
+{
+    /// <summary>
+    /// 在当前服务作用域下创建 <see cref="DefaultHttpContext"/> 实例
+    /// </summary>
+    /// <remarks>解决多线程中获取 <see cref="HttpContext"/> 空问题</remarks>
+    /// <param name="serviceScope"><see cref="IServiceScope"/></param>
+    /// <param name="feature"><see cref="IFeatureCollection"/>，可通过 HttpContext.Features 获取</param>
+    /// <param name="claims"><see cref="ClaimsPrincipal"/>，可通过 HttpContext.User 获取</param>
+    public static void CreateDefaultHttpContext(this IServiceScope serviceScope, IFeatureCollection feature, ClaimsPrincipal claims)
+    {
+        var httpContextAccessor = serviceScope.ServiceProvider.GetService<IHttpContextAccessor>();
+        httpContextAccessor.HttpContext = new DefaultHttpContext(feature)
+        {
+            RequestServices = serviceScope.ServiceProvider,
+            User = claims
+        };
+    }
+}
