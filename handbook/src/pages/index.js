9c8f45bebb8fd2456f9d0605c11ed894040801be
MonkSoul <monksoul@outlook.com>
2023-06-09 17:20:38 +0800

    😊 添加 `Serve.Run` 控制台使用文档 #I7CA5F

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -195,7 +195,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>8,884,687</div>
+            <div style={{ color: "#1fd898" }}>8,888,169</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
