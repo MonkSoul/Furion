3de88e5603a6c4a25d60cb257efb13273b3808fa
MonkSoul <monksoul@outlook.com>
2023-06-12 20:28:39 +0800

    😊 更新 `TPFlow` 赞助商广告图

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -195,7 +195,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>8,930,447</div>
+            <div style={{ color: "#1fd898" }}>8,932,454</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
