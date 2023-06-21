96f78176d3b583becdd93dbc2f262168bd8b6d32
百小僧 <monksoul@outlook.com>
2023-06-21 18:24:01 +0800

    😊 发布 `v4.8.8.28` 版本，改进定时任务休眠控制

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -95,7 +95,7 @@ function Banner() {
               to={useBaseUrl("docs/category/getstart")}
             >
               入门指南
-              <span className="furion-version">v4.8.8.27</span>
+              <span className="furion-version">v4.8.8.28</span>
             </Link>
             <Link
               className="furion-try-demo"
