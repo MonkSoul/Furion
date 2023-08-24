fec308f2511a898f9c8dd6d430d64d961dfb5619
百小僧 <monksoul@outlook.com>
2023-08-25 05:32:36 +0800

    ❤️ 更新用户捐赠列表信息

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -193,7 +193,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>10,000,201</div>
+            <div style={{ color: "#1fd898" }}>10,032,225</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
