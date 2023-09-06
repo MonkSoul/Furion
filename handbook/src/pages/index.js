b1d3ec1358751500c807bffe89097d4f9278d184
百小僧 <monksoul@outlook.com>
2023-09-06 08:20:47 +0800

    😐 更新 `v5`  路线图计划状态

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -193,7 +193,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>10,164,834</div>
+            <div style={{ color: "#1fd898" }}>10,202,251</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
@@ -296,8 +296,8 @@ function WhoUse() {
           <div></div>
           <h1>他们都在用</h1>
           <p>
-            Furion 已在 NuGet
-            平台超1011万次下载。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
+            Furion 已在 NuGet 平台超 1020
+            万次下载。从小型企业到企业的解决方案及知名企业。他们在简单软件和复杂管理系统的开发方面都信任我们。
           </p>
           <a
             className="furion-get-start"
