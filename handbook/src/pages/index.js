315cf3af607b96216cd40957dedad6b93e9415ca
百小僧 <monksoul@outlook.com>
2023-11-04 00:08:23 +0800

    😊 开通 `VIP` 服务企业商家版微信二维码

--- handbook/src/pages/index.js
+++ handbook/src/pages/index.js
@@ -199,7 +199,7 @@ function Gitee() {
             className={"furion-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="furion-log-number">
-            <div style={{ color: "#1fd898" }}>11,288,934</div>
+            <div style={{ color: "#1fd898" }}>11,290,958</div>
             <span className={isDarkTheme ? " dark" : ""}>Downloads</span>
           </div>
         </div>
