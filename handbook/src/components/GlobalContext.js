8ff92ed32de5f7b4c923c4774525bc6f36f61ab8
百小僧 <monksoul@outlook.com>
2023-11-26 11:25:12 +0800

    😊 今日起，文档不支持私有化部署，只允许在线查看

--- handbook/src/components/GlobalContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const GlobalContext = createContext(null);
-
-export default GlobalContext;
--- handbook/src/components/iconfonts/helper.d.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-
-export declare const getIconColor: (color: string | string[] | undefined, index: number, defaultColor: string) => string;
