1d87d07da4c0bbccc09f3300b7855a951c07dd65
百小僧 <monksoul@outlook.com>
2024-04-16 14:07:13 +0800

    😊 新增 动态 `WebAPI` 支持贴 `[Route]` 特性动态生成控制器情况 #I9H1QH

--- framework/Furion/DynamicApiController/Internal/Penetrates.cs
+++ framework/Furion/DynamicApiController/Internal/Penetrates.cs
@@ -107,7 +107,11 @@ internal static class Penetrates
             if (!type.IsPublic || type.IsPrimitive || type.IsValueType || type.IsAbstract || type.IsInterface || type.IsGenericType) return false;
 
             // 继承 ControllerBase 或 实现 IDynamicApiController 的类型 或 贴了 [DynamicApiController] 特性
-            if ((!typeof(Controller).IsAssignableFrom(type) && typeof(ControllerBase).IsAssignableFrom(type)) || typeof(IDynamicApiController).IsAssignableFrom(type) || type.IsDefined(typeof(DynamicApiControllerAttribute), true))
+            if ((!typeof(Controller).IsAssignableFrom(type) && typeof(ControllerBase).IsAssignableFrom(type))
+                || typeof(IDynamicApiController).IsAssignableFrom(type)
+                || type.IsDefined(typeof(DynamicApiControllerAttribute), true)
+                // 支持没有继承 ControllerBase 且贴了 [Route] 特性的情况
+                || (type.IsDefined(typeof(RouteAttribute), true)))
             {
                 // 处理运行时动态生成程序集问题
                 if (type.Assembly?.ManifestModule?.Name == "<Unknown>") return true;
