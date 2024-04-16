376e03e51ead8502b8e4295fe24cb6c9077cfc78
百小僧 <monksoul@outlook.com>
2024-04-16 15:09:08 +0800

    😊 发布 `v4.9.2.19` 版本，改进动态 `WebAPI` 和粘土对象 #I9H1QH #I9H14X

---
+++ samples/Furion.Web.Entry/Controllers/RouteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Furion.Web.Entry.Controllers;
+
+//[ApiController]
+[Route("[controller]")]
+public class RouteController
+{
+    public void Get()
+    {
+    }
+}
