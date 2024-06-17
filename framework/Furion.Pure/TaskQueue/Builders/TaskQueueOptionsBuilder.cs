4d9feb317539f9471a59bd610b1ded170f36bd65
百小僧 <monksoul@outlook.com>
2024-06-17 18:52:02 +0800

    😊 调整 任务队列管道容量，从 `3000` 增加到 `12000`，提升消费吞吐量

--- framework/Furion.Pure/TaskQueue/Builders/TaskQueueOptionsBuilder.cs
+++ framework/Furion.Pure/TaskQueue/Builders/TaskQueueOptionsBuilder.cs
@@ -34,8 +34,8 @@ public sealed class TaskQueueOptionsBuilder
     /// <summary>
     /// 默认内置任务队列内存通道容量
     /// </summary>
-    /// <remarks>超过 n 条待处理消息，第 n+1 条将进入等待，默认为 3000</remarks>
-    public int ChannelCapacity { get; set; } = 3000;
+    /// <remarks>超过 n 条待处理消息，第 n+1 条将进入等待，默认为 12000</remarks>
+    public int ChannelCapacity { get; set; } = 12000;
 
     /// <summary>
     /// 未察觉任务异常事件处理程序
