fdbd8978ef1cadec2e0669430f88f0416e0170df
百小僧 <monksoul@outlook.com>
2024-08-09 17:01:53 +0800

    😊 修复 因移除 `.NET5` 脚手架配置导致脚手架内容缺失 #IAJ128

---
+++ templates/FurionBlazorAppTemplate/src/FurionBlazorApp.EntityFramework.Core/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Furion;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FurionBlazorApp.EntityFramework.Core;
+
+public class Startup : AppStartup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDatabaseAccessor(options =>
+        {
+            options.AddDbPool<DefaultDbContext>();
+        }, "FurionBlazorApp.Database.Migrations");
+    }
+}
