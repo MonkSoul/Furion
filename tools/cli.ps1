731ad9e241b5112a3c80a5912294e4806ae98156
百小僧 <monksoul@outlook.com>
2024-09-02 18:48:48 +0800

    😊 发布 `v4.9.5.6` 版本，新增 `EFCore` 分表插入功能

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.5";
+$FurTools = "Furion Tools v4.9.5.6";
 
 # 输出信息
 $copyright = @"
