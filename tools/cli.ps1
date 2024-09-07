be6361d77494948deee9affbb77cd856c6db6ae9
百小僧 <monksoul@outlook.com>
2024-09-07 18:47:28 +0800

    😊 发布 `v4.9.5.7` 版本，修复 `Swagger` 注解无效和 `EFCore` 更新出现并发异常问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.6";
+$FurTools = "Furion Tools v4.9.5.7";
 
 # 输出信息
 $copyright = @"
