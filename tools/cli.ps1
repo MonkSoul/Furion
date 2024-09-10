0608f85880436fbe58cf211e45345366403831bd
百小僧 <monksoul@outlook.com>
2024-09-10 13:36:55 +0800

    😊 发布 `v4.9.5.8` 版本，改进动态 `WebAPI` 继承时路由生成配置

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.7";
+$FurTools = "Furion Tools v4.9.5.8";
 
 # 输出信息
 $copyright = @"
