cbf96a054a718b189452450c3b9a1cc2f3d3c4e2
百小僧 <monksoul@outlook.com>
2024-09-18 10:24:01 +0800

    😊 发布 `v4.9.5.9` 版本，改进 `EFCore` 增删改操作

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.8";
+$FurTools = "Furion Tools v4.9.5.9";
 
 # 输出信息
 $copyright = @"
