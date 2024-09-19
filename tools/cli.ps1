cf1f3ca881464933dabccc5c99d2ed857118f70e
百小僧 <monksoul@outlook.com>
2024-09-19 11:54:03 +0800

    😊 发布 `v4.9.5.10` 版本，新增 `API` 数据签名和改进视图引擎

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.9";
+$FurTools = "Furion Tools v4.9.5.10";
 
 # 输出信息
 $copyright = @"
