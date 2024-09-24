3d330938507737968388a8fdeb4c7c7718c17867
百小僧 <monksoul@outlook.com>
2024-09-24 17:17:33 +0800

    😊 移除 `Native` 方式创建对象对事件总线的判断代码

--- framework/Furion/App/Native.cs
+++ framework/Furion/App/Native.cs
@@ -58,12 +58,6 @@ public static class Native
     /// <returns></returns>
     public static object CreateInstance(Type windowType, params object[] parameters)
     {
-        // 解决特定案例：控件本身也是事件处理程序：https://gitee.com/dotnetchina/Furion/issues/IAT1JG
-        if (typeof(IEventSubscriber).IsAssignableFrom(windowType))
-        {
-            return App.RootServices.GetServices(typeof(IEventSubscriber)).FirstOrDefault(s => s.GetType() == windowType);
-        }
-
         // 获取构造函数
         var constructors = windowType.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
 
