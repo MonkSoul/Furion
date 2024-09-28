d5eba112f7f99ebba981d8565840e181ac0d70a9
百小僧 <monksoul@outlook.com>
2024-09-29 03:32:42 +0800

    😊 发布 `v4.9.5.13` 版本，解决监听配置文件更改触发多次问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.12";
+$FurTools = "Furion Tools v4.9.5.13";
 
 # 输出信息
 $copyright = @"
